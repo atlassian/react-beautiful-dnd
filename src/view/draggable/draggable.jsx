0f557e036fc5bdedeb5ba13de9540de28883aa4a
Alex Reardon <alexreardon@users.noreply.github.com>
2017-08-20 21:54:14 +1000

    using recommended packages to avoid deprecation warnings (#48)

--- src/view/draggable/draggable.jsx
+++ src/view/draggable/draggable.jsx
@@ -1,5 +1,6 @@
 // @flow
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import memoizeOne from 'memoize-one';
 import invariant from 'invariant';
 import type {
