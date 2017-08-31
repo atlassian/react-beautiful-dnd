857d73b994504438720450ed4cba7fa1cab8276e
Romello Goodman <romellogood@gmail.com>
2017-08-30 21:58:09 -0400

    Following the check for redux devtools on the server that was mentioned here: https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup

--- src/state/create-store.js
+++ src/state/create-store.js
@@ -1,12 +1,14 @@
 // @flow
+/* eslint-disable no-underscore-dangle */
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from './reducer';
 import hookMiddleware from './hook-middleware';
 import type { Store, Hooks } from '../types';
 
-// eslint-disable-next-line no-underscore-dangle
-const composeEnhancers = typeof window !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+const composeEnhancers = typeof window === 'object'
+  && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
 
 export default (hooks: Hooks): Store => createStore(
   reducer,
