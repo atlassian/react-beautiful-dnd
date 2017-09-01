cc2c70f1210e2213ea4cdb812002abe8e51dab67
Alex Reardon <areardon@atlassian.com>
2017-09-01 10:37:02 +1000

    exporting flowtypes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-// not enabling flow for this file
-// it does not cope with this syntax
-// https://github.com/facebook/flow/issues/940
+// @flow
 
-export DragDropContext from './view/drag-drop-context/';
-export Draggable from './view/draggable/';
-export Droppable from './view/droppable/';
+// Ideally would do
+// export DragDropContext from '...'
+// But there is a flow bug that prevents it: https://github.com/facebook/flow/issues/940
+import { DragDropContext as a } from './view/drag-drop-context/';
+import { Droppable as b } from './view/droppable/';
+import { Draggable as c } from './view/draggable/';
+
+export const DragDropContext = a;
+export const Droppable = b;
+export const Draggable = c;
+
+// Public flow types
+
+export type {
+  DraggableId,
+  DroppableId,
+  TypeId,
+
+  // Hooks
+  DragStart,
+  DropResult,
+  DraggableLocation,
+} from './types';
+
+// Droppable
+export type {
+  Provided as DroppableProvided,
+  StateSnapshot as DroppableSnapshot,
+} from './view/droppable/droppable-types';
+
+// Draggable
+export type {
+  Provided as DraggableProvided,
+  StateSnapshot as DraggableSnapshot,
+  DraggableStyle,
+} from './view/draggable/draggable-types';
+
+// DragHandle
+export type {
+  DragHandleProvided,
+} from './view/drag-handle/drag-handle-types';
