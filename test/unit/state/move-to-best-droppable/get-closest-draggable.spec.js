3fb9a5f3c494521a73c0b311bf40f09ebd433838
Alex Reardon <areardon@atlassian.com>
2017-09-08 15:04:46 +1000

    get-closest-draggable now uses a loop to check axis

--- test/unit/state/move-to-best-droppable/get-closest-draggable.spec.js
+++ test/unit/state/move-to-best-droppable/get-closest-draggable.spec.js
@@ -1,7 +1,7 @@
 // @flow
 import getClosestDraggable from '../../../../src/state/move-to-best-droppable/get-closest-draggable';
 import { getDroppableDimension, getDraggableDimension } from '../../../../src/state/dimension';
-import { add, distance } from '../../../../src/state/position';
+import { add, distance, patch } from '../../../../src/state/position';
 import { horizontal, vertical } from '../../../../src/state/axis';
 import getClientRect from '../../../utils/get-client-rect';
 import type {
@@ -13,261 +13,31 @@ import type {
 } from '../../../../src/types';
 
 describe('get closest draggable', () => {
-  describe('on axis axis', () => {
-    const axis: Axis = vertical;
+  [vertical, horizontal].forEach((axis: Axis) => {
+    const start: number = 0;
+    const end: number = 100;
+    const crossAxisStart: number = 0;
+    const crossAxisEnd: number = 20;
 
     const droppable: DroppableDimension = getDroppableDimension({
       id: 'droppable',
       clientRect: getClientRect({
-        top: 0,
-        left: 0,
-        bottom: 100,
-        right: 20,
+        [axis.start]: start,
+        [axis.end]: end,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
       }),
     });
 
-    // first item is partially hidden on the top
-    const partialHiddenUpper: DraggableDimension = getDraggableDimension({
-      id: 'partialHiddenUpper',
-      droppableId: droppable.id,
-      clientRect: getClientRect({
-        top: -10,
-        left: 0,
-        bottom: 20,
-        right: 20,
-      }),
-    });
-
-    const visible1: DraggableDimension = getDraggableDimension({
-      id: 'visible1',
-      droppableId: droppable.id,
-      clientRect: getClientRect({
-        top: 20,
-        left: 0,
-        bottom: 40,
-        right: 20,
-      }),
-    });
-
-    const visible2: DraggableDimension = getDraggableDimension({
-      id: 'visible2',
-      droppableId: droppable.id,
-      clientRect: getClientRect({
-        top: 40,
-        left: 0,
-      // same height as visible1
-        bottom: 60,
-        right: 20,
-      }),
-    });
-
-    // bleeds over the visible boundary
-    const partiallyHiddenLower: DraggableDimension = getDraggableDimension({
-      id: 'partiallyHiddenLower',
-      droppableId: droppable.id,
-      clientRect: getClientRect({
-        top: 60,
-        left: 0,
-        bottom: 120,
-        right: 20,
-      }),
-    });
-
-    // totally invisible
-    const hidden: DraggableDimension = getDraggableDimension({
-      id: 'hidden',
-      droppableId: droppable.id,
-      clientRect: getClientRect({
-        top: 90,
-        left: 0,
-        bottom: 120,
-        right: 20,
-      }),
-    });
-
-    const draggables: DraggableDimensionMap = {
-      [partialHiddenUpper.id]: partialHiddenUpper,
-      [visible1.id]: visible1,
-      [visible2.id]: visible2,
-      [partiallyHiddenLower.id]: partiallyHiddenLower,
-      [hidden.id]: hidden,
-    };
-
-    it('should return the closest draggable', () => {
-      const center1: Position = {
-        x: 100,
-        y: visible1.page.withoutMargin.center.y,
-      };
-      const result1: ?DraggableDimension = getClosestDraggable({
-        axis,
-        pageCenter: center1,
-        destination: droppable,
-        draggables,
-      });
-      expect(result1).toBe(visible1);
-
-    // closest to second
-      const center2: Position = {
-        x: 100,
-        y: visible1.page.withoutMargin.center.y,
-      };
-      const result2: ?DraggableDimension = getClosestDraggable({
-        axis,
-        pageCenter: center2,
-        destination: droppable,
-        draggables,
-      });
-      expect(result2).toBe(visible1);
-    });
-
-    it('should return null if there are no draggables in the droppable', () => {
-      const center: Position = {
-        x: 100,
-        y: 100,
-      };
-      const empty: DraggableDimensionMap = {};
-
-      const result: ?DraggableDimension = getClosestDraggable({
-        axis,
-        pageCenter: center,
-        destination: droppable,
-        draggables: empty,
-      });
-
-      expect(result).toBe(null);
-    });
-
-    describe('removal of draggables that are not entirely within the current visible bounds of a droppable', () => {
-      it('should remove draggables that have upper partial visiblility', () => {
-      // point would usually be closest to first -
-      // but it is outside of the visible bounds of the droppable
-        const center: Position = {
-          x: 100,
-          y: partialHiddenUpper.page.withoutMargin.center.y,
-        };
-
-        const result: ?DraggableDimension = getClosestDraggable({
-          axis,
-          pageCenter: center,
-          destination: droppable,
-          draggables,
-        });
-
-        expect(result).toBe(visible1);
-      });
-
-      it('should remove draggables that have lower partial visiblility', () => {
-        const center: Position = {
-          x: 100,
-          y: partiallyHiddenLower.page.withoutMargin.center.y,
-        };
-
-        const result: ?DraggableDimension = getClosestDraggable({
-          axis,
-          pageCenter: center,
-          destination: droppable,
-          draggables,
-        });
-
-        expect(result).toBe(visible2);
-      });
-
-      it('should remove draggables that have no visiblity', () => {
-        const center: Position = {
-          x: 100,
-          y: hidden.page.withoutMargin.center.y,
-        };
-
-        const result: ?DraggableDimension = getClosestDraggable({
-          axis,
-          pageCenter: center,
-          destination: droppable,
-          draggables,
-        });
-
-        expect(result).toBe(visible2);
-      });
-
-      it('should return null if there are no visible targets', () => {
-        const notVisible: DraggableDimensionMap = {
-          [partialHiddenUpper.id]: partialHiddenUpper,
-          [partiallyHiddenLower.id]: partiallyHiddenLower,
-          [hidden.id]: hidden,
-        };
-        const center: Position = {
-          x: 100,
-          y: 100,
-        };
-
-        const result: ?DraggableDimension = getClosestDraggable({
-          axis,
-          pageCenter: center,
-          destination: droppable,
-          draggables: notVisible,
-        });
-
-        expect(result).toBe(null);
-      });
-    });
-
-    it('should return the draggable that is first on the main axis in the event of a tie', () => {
-    // in this case the distance between visible1 and visible2 is the same
-      const center: Position = {
-        x: 100,
-      // this is a shared edge
-        y: visible2.page.withoutMargin.top,
-      };
-
-      const result: ?DraggableDimension = getClosestDraggable({
-        axis,
-        pageCenter: center,
-        destination: droppable,
-        draggables,
-      });
-
-      expect(result).toBe(visible1);
-
-    // validating test assumptions
-
-    // 1. that they have equal distances
-      expect(distance(center, visible1.page.withoutMargin.center))
-      .toEqual(distance(center, visible2.page.withoutMargin.center));
-
-    // 2. if we move beyond the edge visible2 will be selected
-      const result2: ?DraggableDimension = getClosestDraggable({
-        axis,
-        pageCenter: add(center, { x: 0, y: 1 }),
-        destination: droppable,
-        draggables,
-      });
-      expect(result2).toBe(visible2);
-    });
-  });
-
-  describe('on horizontal axis', () => {
-    const axis: Axis = horizontal;
-    const bottom: number = 20;
-    const top: number = 10;
-
-    const droppable: DroppableDimension = getDroppableDimension({
-      id: 'droppable',
-      clientRect: getClientRect({
-        top,
-        bottom,
-        left: 0,
-        right: 100,
-      }),
-    });
-
-    // first item is partially hidden on the top
+    // first item bleeds backwards past the start of the droppable
     const partialHiddenBackwards: DraggableDimension = getDraggableDimension({
-      id: 'partialHiddenBackwards',
+      id: 'bleedsOverStart',
       droppableId: droppable.id,
       clientRect: getClientRect({
-        top,
-        bottom,
-        left: -10,
-        right: 20,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
+        [axis.start]: -10, // -10
+        [axis.end]: 20,
       }),
     });
 
@@ -275,10 +45,10 @@ describe('get closest draggable', () => {
       id: 'visible1',
       droppableId: droppable.id,
       clientRect: getClientRect({
-        top,
-        bottom,
-        left: 20,
-        right: 40,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
+        [axis.start]: 20,
+        [axis.end]: 40,
       }),
     });
 
@@ -286,22 +56,22 @@ describe('get closest draggable', () => {
       id: 'visible2',
       droppableId: droppable.id,
       clientRect: getClientRect({
-        top,
-        bottom,
-        left: 40,
-        right: 60,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
+        [axis.start]: 40,
+        [axis.end]: 60,
       }),
     });
 
-    // bleeds over the visible boundary
-    const partiallyHiddenForward: DraggableDimension = getDraggableDimension({
-      id: 'partiallyHiddenForward',
+    // bleeds over the end of the visible boundary
+    const partiallyHiddenForwards: DraggableDimension = getDraggableDimension({
+      id: 'bleedsOverEnd',
       droppableId: droppable.id,
       clientRect: getClientRect({
-        top,
-        bottom,
-        left: 60,
-        right: 120,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
+        [axis.start]: 60,
+        [axis.end]: 120,
       }),
     });
 
@@ -310,10 +80,10 @@ describe('get closest draggable', () => {
       id: 'hidden',
       droppableId: droppable.id,
       clientRect: getClientRect({
-        top,
-        bottom,
-        left: 120,
-        right: 140,
+        [axis.crossAxisStart]: crossAxisStart,
+        [axis.crossAxisEnd]: crossAxisEnd,
+        [axis.start]: 120,
+        [axis.end]: 140,
       }),
     });
 
@@ -321,15 +91,15 @@ describe('get closest draggable', () => {
       [partialHiddenBackwards.id]: partialHiddenBackwards,
       [visible1.id]: visible1,
       [visible2.id]: visible2,
-      [partiallyHiddenForward.id]: partiallyHiddenForward,
+      [partiallyHiddenForwards.id]: partiallyHiddenForwards,
       [hidden.id]: hidden,
     };
 
     it('should return the closest draggable', () => {
-      const center1: Position = {
-        y: 100,
-        x: visible1.page.withoutMargin.center.x,
-      };
+      // closet to visible1
+      const center1: Position = patch(
+        axis.line, visible1.page.withoutMargin.center[axis.line], 100
+      );
       const result1: ?DraggableDimension = getClosestDraggable({
         axis,
         pageCenter: center1,
@@ -338,18 +108,17 @@ describe('get closest draggable', () => {
       });
       expect(result1).toBe(visible1);
 
-      // closest to second
-      const center2: Position = {
-        y: 100,
-        x: visible1.page.withoutMargin.center.x,
-      };
+      // closest to visible2
+      const center2: Position = patch(
+        axis.line, visible2.page.withoutMargin.center[axis.line], 100
+      );
       const result2: ?DraggableDimension = getClosestDraggable({
         axis,
         pageCenter: center2,
         destination: droppable,
         draggables,
       });
-      expect(result2).toBe(visible1);
+      expect(result2).toBe(visible2);
     });
 
     it('should return null if there are no draggables in the droppable', () => {
@@ -371,12 +140,11 @@ describe('get closest draggable', () => {
 
     describe('removal of draggables that are not entirely within the current visible bounds of a droppable', () => {
       it('should remove draggables that have backwards partial visiblility', () => {
-      // point would usually be closest to first -
-      // but it is outside of the visible bounds of the droppable
-        const center: Position = {
-          y: 100,
-          x: partialHiddenBackwards.page.withoutMargin.center.x,
-        };
+        // point would usually be closest to visible1 -
+        // but it is outside of the visible bounds of the droppable
+        const center: Position = patch(
+          axis.line, partialHiddenBackwards.page.withoutMargin.center[axis.line], 100
+        );
 
         const result: ?DraggableDimension = getClosestDraggable({
           axis,
@@ -389,10 +157,9 @@ describe('get closest draggable', () => {
       });
 
       it('should remove draggables that have forward partial visiblility', () => {
-        const center: Position = {
-          y: 100,
-          x: partiallyHiddenForward.page.withoutMargin.center.x,
-        };
+        const center: Position = patch(
+          axis.line, partiallyHiddenForwards.page.withoutMargin.center[axis.line], 100
+        );
 
         const result: ?DraggableDimension = getClosestDraggable({
           axis,
@@ -405,10 +172,9 @@ describe('get closest draggable', () => {
       });
 
       it('should remove draggables that have no visiblity', () => {
-        const center: Position = {
-          y: 100,
-          x: hidden.page.withoutMargin.center.x,
-        };
+        const center: Position = patch(
+          axis.line, hidden.page.withoutMargin.center[axis.line], 100
+        );
 
         const result: ?DraggableDimension = getClosestDraggable({
           axis,
@@ -423,12 +189,12 @@ describe('get closest draggable', () => {
       it('should return null if there are no visible targets', () => {
         const notVisible: DraggableDimensionMap = {
           [partialHiddenBackwards.id]: partialHiddenBackwards,
-          [partiallyHiddenForward.id]: partiallyHiddenForward,
+          [partiallyHiddenForwards.id]: partiallyHiddenForwards,
           [hidden.id]: hidden,
         };
         const center: Position = {
-          x: 100,
-          y: 100,
+          x: 0,
+          y: 0,
         };
 
         const result: ?DraggableDimension = getClosestDraggable({
@@ -443,12 +209,13 @@ describe('get closest draggable', () => {
     });
 
     it('should return the draggable that is first on the main axis in the event of a tie', () => {
-    // in this case the distance between visible1 and visible2 is the same
-      const center: Position = {
-        y: 100,
-        // this is a shared edge
-        x: visible2.page.withoutMargin.left,
-      };
+      // in this case the distance between visible1 and visible2 is the same
+      const center: Position = patch(
+        axis.line,
+        // this is shared edge
+        visible2.page.withoutMargin[axis.start],
+        100
+      );
 
       const result: ?DraggableDimension = getClosestDraggable({
         axis,
@@ -461,14 +228,14 @@ describe('get closest draggable', () => {
 
     // validating test assumptions
 
-    // 1. that they have equal distances
+      // 1. that they have equal distances
       expect(distance(center, visible1.page.withoutMargin.center))
       .toEqual(distance(center, visible2.page.withoutMargin.center));
 
       // 2. if we move beyond the edge visible2 will be selected
       const result2: ?DraggableDimension = getClosestDraggable({
         axis,
-        pageCenter: add(center, { x: 1, y: 0 }),
+        pageCenter: add(center, patch(axis.line, 1)),
         destination: droppable,
         draggables,
       });
