65cc0b9036293fa9f8ec816d8ca13ef66390a632
Alex Reardon <alexreardon@users.noreply.github.com>
2017-11-27 14:14:36 +1100

    fixing error in dist build when process.env is not available (#197)

--- rollup.config.js
+++ rollup.config.js
@@ -2,22 +2,29 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
 import yargs from 'yargs';
 
 const args = yargs.argv;
-const external = ['react'];
 
 export default {
   input: './lib/index.js',
   output: {
-    file: `dist/index${args.min ? '.min' : ''}.js`,
+    file: `dist/react-beautiful-dnd${args.min ? '.min' : ''}.js`,
     format: 'umd',
   },
   name: 'ReactBeautifulDnd',
-  plugins: [babel({ exclude: 'node_modules/**', babelrc: false }), resolve(), commonjs()].concat(
+  plugins: [
+    babel({ exclude: 'node_modules/**', babelrc: false }),
+    resolve(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+  ].concat(
     args.min ? uglify() : []
   ),
   sourceMap: false,
-  external,
+  external: ['react'],
   globals: { react: 'React' },
 };
