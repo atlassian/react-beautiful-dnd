94c4718ee255997da8be2714905cea0caa866e40
Alex Reardon <areardon@atlassian.com>
2017-11-27 21:04:25 +1100

    not considering margin when detecting if within a container

--- stories/src/primatives/quote-item.jsx
+++ stories/src/primatives/quote-item.jsx
@@ -25,7 +25,7 @@ cursor: grab;
 box-shadow: ${({ isDragging }) => (isDragging ? `2px 2px 1px ${colors.shadow}` : 'none')};
 padding: ${grid}px;
 min-height: 40px;
-margin-bottom: ${grid}px;
+margin-top: ${grid}px;
 /* TODO: breaks with margin-left (or margin-top?) */
 user-select: none;
 transition: background-color 0.1s ease;
