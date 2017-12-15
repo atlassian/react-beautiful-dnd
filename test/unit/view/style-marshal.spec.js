7efc638006a956b718d678be78ae0d2c0a1caa1d
Alex Reardon <areardon@atlassian.com>
2017-12-15 17:14:33 +1100

    scafold for style marshal tests

---
+++ test/unit/view/style-marshal.spec.js
@@ -0,0 +1,61 @@
+// @flow
+
+describe('style marshal', () => {
+  describe('not dragging', () => {
+    it('should apply the base styles', () => {
+
+    });
+
+    it('should not prevent pointer events or add a transition to the draggable', () => {
+
+    });
+  });
+
+  describe('dragging', () => {
+    it('should apply the base styles', () => {
+
+    });
+
+    it('should block pointer events on the draggables', () => {
+
+    });
+
+    it('should transition transforms on the draggables', () => {
+
+    });
+
+    it('should prevent selecting text', () => {
+
+    });
+
+    it('should apply a grabbing cursor to the body', () => {
+
+    });
+  });
+
+  describe('dropping', () => {
+    it('should remove the dragging styles if a drag is stopped', () => {
+
+    });
+
+    // This will remove pointer-events: none and let the user start dropping other items
+    it('should remove the dragging styles if animating a drop', () => {
+
+    });
+
+    describe('animating cancel', () => {
+      // we do not allow dragging of other items while animating a cancel
+      it('should maintain the dragging styles if animating a cancel', () => {
+
+      });
+
+      it('should clear the dragging style when the animation is finished', () => {
+
+      });
+
+      it('should clear the dragging style when the cancel is flushed at the start of another drag', () => {
+
+      });
+    });
+  });
+});
