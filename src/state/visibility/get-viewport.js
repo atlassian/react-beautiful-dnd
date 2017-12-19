4ccb9c6efc35b9df4bd78eb1a00c9b4f63dfe5aa
Alex Reardon <areardon@atlassian.com>
2017-12-20 07:54:48 +1100

    fixing visibility check for ie11

--- src/state/visibility/get-viewport.js
+++ src/state/visibility/get-viewport.js
@@ -2,8 +2,9 @@
 import type { ClientRect } from '../../types';
 
 export default (): ClientRect => {
-  const top: number = window.scrollY;
-  const left: number = window.scrollX;
+  // would use window.scrollY and window.scrollX but it is not supported in ie11
+  const top: number = window.pageYOffset;
+  const left: number = window.pageXOffset;
   const width: number = window.innerWidth;
   const height: number = window.innerHeight;
 
