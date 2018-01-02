935d262677c6b9ca9cd634b6151bcbb1b8dac921
Alex Reardon <areardon@atlassian.com>
2018-01-03 10:34:05 +1100

    adding droppable scroll test to viewport visiblity filtering

--- test/unit/state/visibility/is-partially-visible.spec.js
+++ test/unit/state/visibility/is-partially-visible.spec.js
@@ -1,7 +1,7 @@
 // @flow
 import getArea from '../../../../src/state/get-area';
 import isPartiallyVisible from '../../../../src/state/visibility/is-partially-visible';
-import { getDroppableDimension } from '../../../../src/state/dimension';
+import { getDroppableDimension, scrollDroppable } from '../../../../src/state/dimension';
 import { offset } from '../../../../src/state/spacing';
 import type {
   Area,
@@ -20,7 +20,7 @@ const viewport: Area = getArea({
 
 const asBigAsViewport: DroppableDimension = getDroppableDimension({
   descriptor: {
-    id: 'huge',
+    id: 'same-as-viewport',
     type: 'TYPE',
   },
   client: viewport,
@@ -57,48 +57,116 @@ const smallDroppable: DroppableDimension = getDroppableDimension({
 
 describe('is partially visible', () => {
   describe('viewport', () => {
-    it('should return false if the item is not in the viewport', () => {
-      expect(isPartiallyVisible({
-        target: notInViewport,
-        viewport,
-        destination: asBigAsViewport,
-      })).toBe(false);
-    });
-
-    it('should return true if item takes up entire viewport', () => {
-      expect(isPartiallyVisible({
-        target: viewport,
-        viewport,
-        destination: asBigAsViewport,
-      })).toBe(true);
-    });
-
-    it('should return true if the item is totally visible in the viewport', () => {
-      expect(isPartiallyVisible({
-        target: inViewport1,
-        viewport,
-        destination: asBigAsViewport,
-      })).toBe(true);
-    });
-
-    it('should return true if the item is partially visible in the viewport', () => {
-      const partials: Spacing[] = [
-        // bleed over top
-        offset(viewport, { x: 0, y: -1 }),
-        // bleed over right
-        offset(viewport, { x: 1, y: 0 }),
-        // bleed over bottom
-        offset(viewport, { x: 0, y: 1 }),
-        // bleed over left
-        offset(viewport, { x: -1, y: 0 }),
-      ];
-
-      partials.forEach((partial: Spacing) => {
+    describe('without changes in droppable scroll', () => {
+      it('should return false if the item is not in the viewport', () => {
         expect(isPartiallyVisible({
-          target: partial,
+          target: notInViewport,
+          viewport,
+          destination: asBigAsViewport,
+        })).toBe(false);
+      });
+
+      it('should return true if item takes up entire viewport', () => {
+        expect(isPartiallyVisible({
+          target: viewport,
           viewport,
           destination: asBigAsViewport,
         })).toBe(true);
+      });
+
+      it('should return true if the item is totally visible in the viewport', () => {
+        expect(isPartiallyVisible({
+          target: inViewport1,
+          viewport,
+          destination: asBigAsViewport,
+        })).toBe(true);
+      });
+
+      it('should return true if the item is partially visible in the viewport', () => {
+        const partials: Spacing[] = [
+          // bleed over top
+          offset(viewport, { x: 0, y: -1 }),
+          // bleed over right
+          offset(viewport, { x: 1, y: 0 }),
+          // bleed over bottom
+          offset(viewport, { x: 0, y: 1 }),
+          // bleed over left
+          offset(viewport, { x: -1, y: 0 }),
+        ];
+
+        partials.forEach((partial: Spacing) => {
+          expect(isPartiallyVisible({
+            target: partial,
+            viewport,
+            destination: asBigAsViewport,
+          })).toBe(true);
+        });
+      });
+    });
+
+    describe('with changes in droppable scroll', () => {
+      const clippedByViewport: DroppableDimension = getDroppableDimension({
+        descriptor: {
+          id: 'clipped',
+          type: 'TYPE',
+        },
+        client: getArea({
+          top: viewport.top,
+          // stretches out the bottom of the viewport
+          bottom: viewport.bottom + 100,
+          left: viewport.left,
+          right: viewport.right,
+        }),
+      });
+
+      describe('originally invisible but now invisible', () => {
+        it('should take into account the droppable scroll when detecting visibility', () => {
+          const originallyInvisible: Spacing = {
+            top: viewport.bottom + 1,
+            bottom: viewport.bottom + 100,
+            right: viewport.left + 1,
+            left: viewport.left + 100,
+          };
+
+          // originally invisible
+          expect(isPartiallyVisible({
+            target: originallyInvisible,
+            destination: clippedByViewport,
+            viewport,
+          })).toBe(false);
+
+          // after scroll the target is now visible
+          expect(isPartiallyVisible({
+            target: originallyInvisible,
+            destination: scrollDroppable(clippedByViewport, { x: 0, y: 100 }),
+            viewport,
+          })).toBe(true);
+        });
+      });
+
+      describe('originally visible but now visible', () => {
+        it('should take into account the droppable scroll when detecting visibility', () => {
+          const originallyVisible: Spacing = {
+            top: viewport.top,
+            bottom: viewport.top + 50,
+            right: viewport.left + 1,
+            left: viewport.left + 100,
+          };
+
+          // originally visible
+          expect(isPartiallyVisible({
+            target: originallyVisible,
+            destination: clippedByViewport,
+            viewport,
+          })).toBe(true);
+
+          // after scroll the target is now invisible
+          expect(isPartiallyVisible({
+            target: originallyVisible,
+            destination: scrollDroppable(clippedByViewport, { x: 0, y: 100 }),
+            viewport,
+          })).toBe(false);
+        });
       });
     });
   });
@@ -164,6 +232,9 @@ describe('is partially visible', () => {
           })).toBe(true);
         });
       });
+    });
+
+    describe('with changes in droppable scroll', () => {
     });
   });
 
