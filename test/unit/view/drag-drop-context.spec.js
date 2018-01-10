b3716059ec14722f46415232587b79142a028373
Alex Reardon <alexreardon@users.noreply.github.com>
2018-01-11 09:04:52 +1100

    adding more tests for consumer redux usage (#251)

--- test/unit/view/drag-drop-context.spec.js
+++ test/unit/view/drag-drop-context.spec.js
@@ -1,9 +1,13 @@
 // @flow
+/* eslint-disable react/no-multi-comp */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import TestUtils from 'react-dom/test-utils';
 import { mount } from 'enzyme';
-import { DragDropContext } from '../../../src/';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+import { Droppable, Draggable, DragDropContext } from '../../../src/';
+import type { DraggableProvided, DroppableProvided } from '../../../src/';
 import { storeKey, canLiftContextKey } from '../../../src/view/context-keys';
 
 class App extends Component<*> {
@@ -71,6 +75,100 @@ describe('DragDropContext', () => {
       const app = TestUtils.findRenderedComponentWithType(tree, App);
 
       expect(app.context[canLiftContextKey]).toBeInstanceOf(Function);
+    });
+  });
+
+  describe('Playing with other redux apps', () => {
+    type ExternalState = {|
+      foo: string,
+    |}
+    const original: ExternalState = {
+      foo: 'bar',
+    };
+    // super boring reducer that always returns the same thing
+    const reducer = (state: ExternalState = original) => state;
+    const store = createStore(reducer);
+
+    class Unconnected extends Component<ExternalState> {
+      render() {
+        return <div>{this.props.foo}</div>;
+      }
+    }
+
+    const Connected = connect((state: ExternalState): ExternalState => state)(Unconnected);
+
+    it('should avoid clashes with parent redux applications', () => {
+      class Container extends Component<*> {
+        render() {
+          return (
+            <Provider store={store}>
+              <DragDropContext onDragEnd={() => { }}>
+                <Droppable droppableId="droppable">
+                  {(droppableProvided: DroppableProvided) => (
+                    <div ref={droppableProvided.innerRef}>
+                      <Draggable draggableId="draggableId">
+                        {(draggableProvided: DraggableProvided) => (
+                          <div>
+                            <div
+                              ref={draggableProvided.innerRef}
+                              {...draggableProvided.dragHandleProps}
+                              {...draggableProvided.draggableProps}
+                            >
+                              <Connected />
+                            </div>
+                            {draggableProvided.placeholder}
+                          </div>
+                        )}
+                      </Draggable>
+                      {droppableProvided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </DragDropContext>
+            </Provider>
+          );
+        }
+      }
+      const wrapper = mount(<Container />);
+
+      expect(wrapper.find(Container).text()).toBe(original.foo);
+    });
+
+    it('should avoid clashes with child redux applications', () => {
+      class Container extends Component<*> {
+        render() {
+          return (
+            <DragDropContext onDragEnd={() => { }}>
+              <Droppable droppableId="droppable">
+                {(droppableProvided: DroppableProvided) => (
+                  <div ref={droppableProvided.innerRef}>
+                    <Draggable draggableId="draggableId">
+                      {(draggableProvided: DraggableProvided) => (
+                        <div>
+                          <div
+                            ref={draggableProvided.innerRef}
+                            {...draggableProvided.dragHandleProps}
+                            {...draggableProvided.draggableProps}
+                          >
+                            <Provider store={store}>
+                              <Connected />
+                            </Provider>
+                          </div>
+                          {draggableProvided.placeholder}
+                        </div>
+                      )}
+                    </Draggable>
+                    {droppableProvided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          );
+        }
+      }
+      const wrapper = mount(<Container />);
+
+      expect(wrapper.find(Container).text()).toBe(original.foo);
     });
   });
 });
