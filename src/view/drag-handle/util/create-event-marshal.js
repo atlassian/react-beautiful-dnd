6e167b07394c30cf554b3a05627f9b972d27b036
Alex Reardon <areardon@atlassian.com>
2018-03-12 08:10:14 +1100

    simplier event marshal

--- src/view/drag-handle/util/create-event-marshal.js
+++ src/view/drag-handle/util/create-event-marshal.js
@@ -8,41 +8,24 @@ export type EventMarshal = {|
 
 export default (): EventMarshal => {
   let isMouseDownHandled: boolean = false;
-  let handleTimeoutId: ?number = null;
-
-  const isHandled = (): boolean => isMouseDownHandled;
-
-  const clearTimeoutIfNeeded = (): void => {
-    if (handleTimeoutId == null) {
-      return;
-    }
-
-    clearTimeout(handleTimeoutId);
-    handleTimeoutId = null;
-  };
-
-  const reset = (): void => {
-    clearTimeoutIfNeeded();
-    isMouseDownHandled = false;
-  };
 
   const handle = (): void => {
     if (isMouseDownHandled) {
       console.error('Cannot handle mouse down as it is already handled');
       return;
     }
-
     isMouseDownHandled = true;
+  };
 
-    clearTimeoutIfNeeded();
-    handleTimeoutId = setTimeout(reset);
+  const isHandled = (): boolean => isMouseDownHandled;
+
+  const reset = (): void => {
+    isMouseDownHandled = false;
   };
 
   return {
     handle,
     isHandled,
-    // This is not strictly needed given that we use the setTimeout to clear this
-    // However, add it makes testing way more explicit
     reset,
   };
 };
