5c70f20ce74c54b3c4d681fca869f3d73278eb17
Ben Conolly <ben@thinkmill.com.au>
2018-03-20 12:25:56 +1100

    formatting for website

--- website/data/SiteConfig.js
+++ website/data/SiteConfig.js
@@ -1,53 +1,54 @@
+// @flow
 module.exports = {
-  blogPostDir: "posts", // The name of directory that contains your posts.
-  lessonsDir: "lessons", // The name of the directory that contains lessons or docs.
-  siteTitle: "Gatsby Docs Starter", // Site title.
-  siteTitleAlt: "Gatsby Starter Template for Creating Docs", // Alternative site title for SEO.
-  siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://ericwindmill.com", // Domain of your website without pathPrefix.
-  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: "A GatsbyJS stater for creating Documentation or Tutorials.", // Website description used for RSS feeds/meta description tag.
-  siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  googleAnalyticsID: "UA-82450300-1 ", // GA tracking ID.
-  disqusShortname: "https-ericwindmill-github-io-gatsby-advanced-starter", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
-  userName: "User", // Username to display in the author segment.
-  userTwitter: "ericwindmill", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Portland, OR", // User location to display in the author segment.
-  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
-  userDescription: "All about me!", // User description to display in the author segment.
+  blogPostDir: 'posts', // The name of directory that contains your posts.
+  lessonsDir: 'lessons', // The name of the directory that contains lessons or docs.
+  siteTitle: 'Gatsby Docs Starter', // Site title.
+  siteTitleAlt: 'Gatsby Starter Template for Creating Docs', // Alternative site title for SEO.
+  siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
+  siteUrl: 'https://ericwindmill.com', // Domain of your website without pathPrefix.
+  pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: 'A GatsbyJS stater for creating Documentation or Tutorials.', // Website description used for RSS feeds/meta description tag.
+  siteRss: '/rss.xml', // Path to the RSS file.
+  siteFBAppID: '1825356251115265', // FB Application ID for using app insights
+  googleAnalyticsID: 'UA-82450300-1 ', // GA tracking ID.
+  disqusShortname: 'https-ericwindmill-github-io-gatsby-advanced-starter', // Disqus shortname.
+  postDefaultCategoryID: 'Tech', // Default category for posts.
+  userName: 'User', // Username to display in the author segment.
+  userTwitter: 'ericwindmill', // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: 'Portland, OR', // User location to display in the author segment.
+  userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
+  userDescription: 'All about me!', // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
-      label: "GitHub",
-      url: "https://github.com/ericwindmill",
-      iconClassName: "fa fa-github"
+      label: 'GitHub',
+      url: 'https://github.com/ericwindmill',
+      iconClassName: 'fa fa-github',
     },
     {
-      label: "Twitter",
-      url: "https://twitter.com/ericwindmill",
-      iconClassName: "fa fa-twitter"
+      label: 'Twitter',
+      url: 'https://twitter.com/ericwindmill',
+      iconClassName: 'fa fa-twitter',
     },
     {
-      label: "Messenger",
-      url: "https://www.messenger.com/t/eric.windmill",
-      iconClassName: "fa fa-message"
+      label: 'Messenger',
+      url: 'https://www.messenger.com/t/eric.windmill',
+      iconClassName: 'fa fa-message',
     },
     {
-      label: "Email",
-      url: "mailto:eric@ericwindmill.com",
-      iconClassName: "fa fa-envelope"
+      label: 'Email',
+      url: 'mailto:eric@ericwindmill.com',
+      iconClassName: 'fa fa-envelope',
     },
     {
-      label: "Instagram",
-      url: "https://instagram.com/ericwindmill",
-      iconClassName: "fa fa-envelope"
-    }
+      label: 'Instagram',
+      url: 'https://instagram.com/ericwindmill',
+      iconClassName: 'fa fa-envelope',
+    },
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
-  themeColor: "#c62828", // Used for setting manifest and progress theme colors.
-  backgroundColor: "#e0e0e0", // Used for setting manifest background color.
+  copyright: 'Copyright © 2017. Advanced User', // Copyright string for the footer of the website and RSS feed.
+  themeColor: '#c62828', // Used for setting manifest and progress theme colors.
+  backgroundColor: '#e0e0e0', // Used for setting manifest background color.
   // TODO: Move this literally anywhere better.
-  toCChapters: ["", "Chapter 1", "Chapter 2"] // Used to generate the Table Of Contents. Index 0 should be blank.
+  toCChapters: ['', 'Chapter 1', 'Chapter 2'], // Used to generate the Table Of Contents. Index 0 should be blank.
 };
--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,6 +1,7 @@
-const config = require("./data/SiteConfig");
+// @flow
+const config = require('./data/SiteConfig');
 
-const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
 
 module.exports = {
   pathPrefix: config.pathPrefix,
@@ -13,66 +14,66 @@ module.exports = {
       description: config.siteDescription,
       image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
       author: config.userName,
-      copyright: config.copyright
-    }
+      copyright: config.copyright,
+    },
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
-        fonts: [`crimson text:400, 400i, 700, 700i`,
-          `space mono:400,700`,
+        fonts: ['crimson text:400, 400i, 700, 700i',
+          'space mono:400,700',
         ],
-      }
+      },
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "posts",
-        path: `${__dirname}/content/`
-      }
+        name: 'posts',
+        path: `${__dirname}/content/`,
+      },
     },
     {
-      resolve: "gatsby-transformer-remark",
+      resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
           {
-            resolve: "gatsby-remark-images",
+            resolve: 'gatsby-remark-images',
             options: {
-              maxWidth: 690
-            }
+              maxWidth: 690,
+            },
           },
           {
-            resolve: "gatsby-remark-responsive-iframe"
+            resolve: 'gatsby-remark-responsive-iframe',
           },
-          "gatsby-remark-prismjs",
-          "gatsby-remark-copy-linked-files",
-          "gatsby-remark-autolink-headers",
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-autolink-headers',
 
-        ]
-      }
+        ],
+      },
     },
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: config.googleAnalyticsID
-      }
+        trackingId: config.googleAnalyticsID,
+      },
     },
     {
-      resolve: "gatsby-plugin-nprogress",
+      resolve: 'gatsby-plugin-nprogress',
       options: {
-        color: config.themeColor
-      }
+        color: config.themeColor,
+      },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-catch-links",
-    "gatsby-plugin-twitter",
-    "gatsby-plugin-sitemap",
-    "gatsby-transformer-json",
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-twitter',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-json',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
         name: config.siteTitle,
         short_name: config.siteTitle,
@@ -80,29 +81,29 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: "minimal-ui",
+        display: 'minimal-ui',
         icons: [
           {
-            src: "/logos/logo-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
+            src: '/logos/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
           },
           {
-            src: "/logos/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
+            src: '/logos/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     },
-    "gatsby-plugin-offline",
+    'gatsby-plugin-offline',
     {
-      resolve: "gatsby-plugin-feed",
+      resolve: 'gatsby-plugin-feed',
       options: {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "GatsbyJS Material Starter";
+          ret.generator = 'GatsbyJS Material Starter';
           return ret;
         },
         query: `
@@ -134,7 +135,7 @@ module.exports = {
                 author: rssMetadata.author,
                 url: rssMetadata.site_url + edge.node.fields.slug,
                 guid: rssMetadata.site_url + edge.node.fields.slug,
-                custom_elements: [{ "content:encoded": edge.node.html }]
+                custom_elements: [{ 'content:encoded': edge.node.html }],
               }));
             },
             query: `
@@ -161,10 +162,10 @@ module.exports = {
               }
             }
           `,
-            output: config.siteRss
-          }
-        ]
-      }
-    }
-  ]
+            output: config.siteRss,
+          },
+        ],
+      },
+    },
+  ],
 };
