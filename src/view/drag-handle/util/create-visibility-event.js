703159bacb75c586c79b576f35c1a90d07435602
grady-lad <ogradymartin7@gmail.com>
2018-03-21 21:05:43 +0000

    Support for visibility change event for different browsers

--- src/view/drag-handle/util/create-visibility-event.js
+++ src/view/drag-handle/util/create-visibility-event.js
@@ -1,31 +1,39 @@
 // @flow
 import type { EventBinding } from './event-types';
 
-const getHiddenPrefix = () : ?string => {
+const getVisibiltyEvent = () : ?{hidden:string, name:string} => {
   const prefixes = ['ms', 'webkit', 'moz', 'o'];
   let browserPrefix;
   prefixes.forEach((prefix) => {
     if (`${prefix}Hidden` in document) {
-      browserPrefix = `${prefix}Hidden`;
+      browserPrefix = {
+        hidden: `${prefix}Hidden`,
+        name: `${prefix}visibilitychange`,
+      };
     }
   });
   if (!browserPrefix && 'hidden' in document) { // Opera 12.10 and Firefox 18 and later support
-    return 'hidden';
+    return {
+      hidden: 'hidden',
+      name: 'visibilitychange',
+    };
   }
   return browserPrefix;
 };
 
 export default (cancel: Function = () => {}) : EventBinding => {
   if (typeof window !== 'undefined') {
-    const hidden = getHiddenPrefix();
-    return {
-      eventName: 'visibilitychange',
-      fn: () => {
-        if (document[hidden]) {
-          cancel();
-        }
-      },
-    };
+    const visEvent = getVisibiltyEvent();
+    if (visEvent) {
+      return {
+        eventName: visEvent.name,
+        fn: () => {
+          if (document[visEvent.hidden]) {
+            cancel();
+          }
+        },
+      };
+    }
   }
   return {
     eventName: '',
