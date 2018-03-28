8cc51879df6ca9b5376a42b4e66cffc3127210cb
Raja Bellebon <rbellebon@atlassian.com>
2018-03-29 09:00:45 +1100

    Remove css file

--- test/utils/css.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-const singleListContainer = '[data-react-beautiful-dnd-droppable="0"]';
-const firstCard = '#root div > div:nth-child(1) > a';
-const secondCard = '#root div > div:nth-child(2) > a';
-
-module.exports = { singleListContainer, firstCard, secondCard };
