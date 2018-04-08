497248d68a6516768d771e073cc970d2e02c4c05
Bogdan Chadkin <trysound@yandex.ru>
2018-04-08 11:25:15 +0300

    Add size snapshot of production umd build

--- rollup.config.js
+++ rollup.config.js
@@ -8,41 +8,36 @@ import replace from 'rollup-plugin-replace';
 import strip from 'rollup-plugin-strip';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
+const input = './src/index.js';
+
+const extensions = ['.js', '.jsx'];
+
+const getBabelOptions = () => ({
+  exclude: 'node_modules/**',
+  runtimeHelpers: true,
+});
+
 const checkSnapshot = process.env.SNAPSHOT === 'check';
 
 const getUMDConfig = ({ env, file }) => {
   const config = {
-    input: './src/index.js',
+    input,
     output: {
       file,
       format: 'umd',
       name: 'ReactBeautifulDnd',
       globals: { react: 'React' },
     },
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        runtimeHelpers: true,
-      }),
-      resolve({
-        extensions: ['.js', '.jsx'],
-      }),
-      commonjs({
-        include: 'node_modules/**',
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(env),
-      }),
-      strip({
-        debugger: true,
-      }),
-    ],
     external: ['react'],
+    plugins: [
+      babel(getBabelOptions()),
+      resolve({ extensions }),
+      commonjs({ include: 'node_modules/**' }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+      strip({ debugger: true }),
+      sizeSnapshot({ updateSnapshot: !checkSnapshot }),
+    ],
   };
-
-  if (env === 'development') {
-    config.plugins.push(sizeSnapshot({ updateSnapshot: !checkSnapshot }));
-  }
 
   if (env === 'production') {
     config.plugins.push(uglify());
