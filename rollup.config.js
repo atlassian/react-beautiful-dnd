54708dc36a806553a3ae903879811f70c9e1c244
Bogdan Chadkin <trysound@yandex.ru>
2018-04-11 02:26:03 +0300

    Provide cjs and esm bundles with information about treeshakability (#429)

--- rollup.config.js
+++ rollup.config.js
@@ -8,9 +8,15 @@ import replace from 'rollup-plugin-replace';
 import strip from 'rollup-plugin-strip';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
+const pkg = require('./package.json');
+
 const input = './src/index.js';
 
 const extensions = ['.js', '.jsx'];
+
+// Treat as externals all not relative and not absolute paths
+// e.g. 'react'
+const isExternal = id => !id.startsWith('.') && !id.startsWith('/');
 
 const getBabelOptions = () => ({
   exclude: 'node_modules/**',
@@ -19,11 +25,11 @@ const getBabelOptions = () => ({
 
 const checkSnapshot = process.env.SNAPSHOT === 'check';
 
-const getUMDConfig = ({ env, file }) => {
-  const config = {
+export default [
+  {
     input,
     output: {
-      file,
+      file: 'dist/react-beautiful-dnd.umd.js',
       format: 'umd',
       name: 'ReactBeautifulDnd',
       globals: { react: 'React' },
@@ -33,20 +39,49 @@ const getUMDConfig = ({ env, file }) => {
       babel(getBabelOptions()),
       resolve({ extensions }),
       commonjs({ include: 'node_modules/**' }),
-      replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
-      strip({ debugger: true }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
       sizeSnapshot({ updateSnapshot: !checkSnapshot }),
     ],
-  };
+  },
 
-  if (env === 'production') {
-    config.plugins.push(uglify());
-  }
+  {
+    input,
+    output: {
+      file: 'dist/react-beautiful-dnd.min.js',
+      format: 'umd',
+      name: 'ReactBeautifulDnd',
+      globals: { react: 'React' },
+    },
+    external: ['react'],
+    plugins: [
+      babel(getBabelOptions()),
+      resolve({ extensions }),
+      commonjs({ include: 'node_modules/**' }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      strip({ debugger: true }),
+      sizeSnapshot({ updateSnapshot: !checkSnapshot }),
+      uglify(),
+    ],
+  },
 
-  return config;
-};
+  {
+    input,
+    output: { file: pkg.main, format: 'cjs' },
+    external: isExternal,
+    plugins: [
+      resolve({ extensions }),
+      babel(getBabelOptions()),
+    ],
+  },
 
-export default [
-  getUMDConfig({ env: 'development', file: 'dist/react-beautiful-dnd.js' }),
-  getUMDConfig({ env: 'production', file: 'dist/react-beautiful-dnd.min.js' }),
+  {
+    input,
+    output: { file: pkg.module, format: 'es' },
+    external: isExternal,
+    plugins: [
+      resolve({ extensions }),
+      babel(getBabelOptions()),
+      sizeSnapshot({ updateSnapshot: !checkSnapshot }),
+    ],
+  },
 ];
