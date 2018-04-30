4587975a34fa3e9318685146555ab520fe7e4889
Alex Reardon <areardon@atlassian.com>
2018-04-30 21:50:52 +1000

    sidebar now conditional based on screen width

--- website/src/components/PageWrapper.jsx
+++ website/src/components/PageWrapper.jsx
@@ -2,42 +2,111 @@
 import React, { type Node } from 'react';
 import styled from 'styled-components';
 import Link from 'gatsby-link';
+import Media from 'react-media';
 import { colors as akColors } from '@atlaskit/theme';
-import Sidebar from './Sidebar';
+import Sidebar from './sidebar';
 import type { sitePage, docsPage } from './types';
-import { colors, grid, sidebarWidth } from '../constants';
+import { singleColumn } from '../components/media';
+import { grid, sidebarWidth } from '../constants';
+
+const gutter: number = grid * 2;
 
 const Content = styled.div`
-
-  margin-left: ${sidebarWidth + (grid * 2)}px;
+  margin-left: ${sidebarWidth + gutter}px;
+  margin-right: ${gutter}px;
   margin-top ${grid * 4}px;
   display: flex;
   justify-content: center;
+
+  ${singleColumn.fn`
+    margin-left: 16px;
+  `}
 `;
 
 const ContentSpacing = styled.div`
-background: lightgreen;
+  background: lightgreen;
   max-width: 960px;
   width: 100%;
   min-height: 100vh;
 `;
 
-type Props = {
+type MobileTopBarProps = {|
+  onMenuToggle: () => void,
+|}
+class MobileTopBar extends React.Component<MobileTopBarProps> {
+  render() {
+    return (
+      <div>
+        Mobile topbar
+        <button onClick={this.props.onMenuToggle}>Toggle menu</button>
+      </div>
+    );
+  }
+}
+
+type ExternalProps = {|
   examples: sitePage,
   docs: docsPage,
   internal: sitePage,
   showInternal: boolean,
   children: Node,
+|}
+
+type InternalProps = {|
+  ...ExternalProps,
+  isInSingleColumn: boolean,
+|}
+
+type State = {|
+  showSidebar: boolean,
+|}
+
+class WithConditionalSidebar extends React.Component<InternalProps, State> {
+  static getDerivedStateFromProps = (nextProps: InternalProps): State => ({
+    showSidebar: !nextProps.isInSingleColumn,
+  })
+
+  state: State = {
+    showSidebar: !this.props.isInSingleColumn,
+  };
+
+  onMenuToggle = () => {
+    this.setState({
+      showSidebar: !this.state.showSidebar,
+    });
+  }
+
+  render() {
+    const { examples, docs, internal, showInternal, children, isInSingleColumn } = this.props;
+    const { showSidebar } = this.state;
+
+    const sidebar: Node = showSidebar ? (
+      <Sidebar
+        examples={examples}
+        docs={docs}
+        internal={internal}
+        showInternal={showInternal}
+      />) : null;
+
+    const topbar: Node = isInSingleColumn ?
+      <MobileTopBar onMenuToggle={this.onMenuToggle} /> : null;
+
+    return (
+      <React.Fragment>
+        {sidebar}
+        {topbar}
+        <Content>
+          <ContentSpacing>
+            {children}
+          </ContentSpacing>
+        </Content>
+      </React.Fragment>
+    );
+  }
 }
 
-export default ({ examples, docs, internal, showInternal, children }: Props) => (
-  <React.Fragment>
-    {/* <Header><HeaderLink to="/" href="/">React-Beautiful-Dnd</HeaderLink></Header> */}
-    <Sidebar examples={examples} docs={docs} internal={internal} showInternal={showInternal} />
-    <Content>
-      <ContentSpacing>
-        {children}
-      </ContentSpacing>
-    </Content>
-  </React.Fragment>
+export default (props: ExternalProps) => (
+  <Media query={singleColumn.negatedQuery}>
+    {(matches: boolean) => <WithConditionalSidebar {...props} isInSingleColumn={!matches} />}
+  </Media>
 );
