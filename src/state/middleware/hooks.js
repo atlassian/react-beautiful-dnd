ce2e8ad226a9dfb1dd2f416ec31c81ee01f67693
Alex Reardon <areardon@atlassian.com>
2018-05-10 15:17:35 +1000

    adding hook placeholder

--- src/state/middleware/hooks.js
+++ src/state/middleware/hooks.js
@@ -4,13 +4,33 @@ import type { Store, State, Action, Hooks } from '../../types';
 import type { StyleMarshal } from '../../view/style-marshal/style-marshal-types';
 
 export default (getHooks: () => Hooks, announce: Announce) => {
-  const hookCaller: HookCaller = createHookCaller(announce);
-  return (store: Store) => (next: (Action) => mixed) => (action: Action): mixed => {
-    const state: State = store.getState();
+  (store: Store) => (next: (Action) => mixed) => (action: Action): mixed => {
+    // on drag start
+    if (action.type === 'INITIAL_PUBLISH') {
 
-    // TODO:
-    // hookCaller.onStateChange(getHooks(), )
-    return next(action);
+    }
+
+    // On drag end?
+    if (action.type === 'CLEAN') {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO: Perform hooks after releasing action?
+    next(action);
+
+
+
+
   };
-};
 
