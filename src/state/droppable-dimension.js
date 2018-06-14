2add0446848327e94ccdc79bc25c519226016c54
Alex Reardon <areardon@atlassian.com>
2018-06-14 15:19:11 +1000

    placeholder

--- src/state/droppable-dimension.js
+++ src/state/droppable-dimension.js
@@ -165,3 +165,13 @@ export const scrollDroppable = (
   };
   return result;
 };
+
+// TODO
+const growSubjectIfNeeded = ({
+  draggables: DraggableDimensionMap,
+  droppable: DroppableDimension,
+  addition: Position,
+}): DroppableDimension => {
+
+};
+
