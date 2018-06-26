2f52f188fa6dc6667d08b652d43926d258013146
Alex Reardon <areardon@atlassian.com>
2018-06-26 15:08:40 +1000

    initial rule changes

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:flowtype/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/flowtype',
+    'prettier/react',
+    'plugin:prettier/recommended',
+  ],
+  parser: 'babel-eslint',
+  plugins: ['react', 'jsx-a11y', 'jest', 'flowtype', 'import', 'prettier'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    'jest/globals': true,
+  },
+  globals: {
+    // flow globals
+    TimeoutID: true,
+    IntervalID: true,
+    AnimationFrameID: true,
+  },
+  rules: {
+    // Allowing warning and error console logging
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+};
