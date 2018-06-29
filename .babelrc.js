33c616ec032f3573247b087ae2a35205558a0a9f
Bogdan Chadkin <trysound@yandex.ru>
2018-06-29 14:22:25 +0300

    Babel7 (#590)
    * Fix uglify and update size snapshot
    
    * Upgrade to babel7
    
    * Enable loose mode for class properties
    
    * Enable loose mode for object rest spread
    
    * Provide esm babel helpers for esm bundle
    
    * Ignore puppeteer test
    
    * adding babelrc just for the storybook
    
    * adding some notes about the storybook setup
    
    * renaming readme
    
    * updating lock file

---
+++ .babelrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: [
+    '@babel/react',
+    '@babel/flow',
+    ['@babel/env', { modules: false, loose: true }],
+  ],
+  plugins: [
+    ['@babel/proposal-class-properties', { loose: true }],
+    ['@babel/proposal-object-rest-spread', { loose: true }],
+  ],
+  comments: false,
+};
+
+if (process.env.NODE_ENV === 'test') {
+  module.exports.plugins.push('@babel/transform-modules-commonjs');
+}
