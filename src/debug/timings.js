cb5767ac4c7685c5ac70995cab40764b4c0f838a
Bogdan Chadkin <trysound@yandex.ru>
2018-07-02 01:02:22 +0300

    Eliminate timings in production bundle
    Inlining `NODE_ENV` comparison allows uglify to eliminate
    unused code in production.

--- src/debug/timings.js
+++ src/debug/timings.js
@@ -9,25 +9,22 @@ const records: Records = {};
 
 const flag: string = '__react-beautiful-dnd-debug-timings-hook__';
 
-// we want to strip all the code out for production builds
-// draw back: can only do timings in dev env (which seems to be fine for now)
-const isProduction: boolean = process.env.NODE_ENV === 'production';
-
 const isTimingsEnabled = (): boolean => Boolean(window[flag]);
 
 // Debug: uncomment to enable
 // window[flag] = true;
 
 export const start = (key: string) => {
-  if (isProduction) {
-    return;
-  }
-  if (!isTimingsEnabled()) {
-    return;
-  }
-  const now: number = performance.now();
+  // we want to strip all the code out for production builds
+  // draw back: can only do timings in dev env (which seems to be fine for now)
+  if (process.env.NODE_ENV !== 'production') {
+    if (!isTimingsEnabled()) {
+      return;
+    }
+    const now: number = performance.now();
 
-  records[key] = now;
+    records[key] = now;
+  }
 };
 
 type Style = {|
@@ -36,50 +33,49 @@ type Style = {|
 |};
 
 export const finish = (key: string) => {
-  if (isProduction) {
-    return;
-  }
-  if (!isTimingsEnabled()) {
-    return;
-  }
-  const now: number = performance.now();
-
-  const previous: ?number = records[key];
-
-  invariant(previous, 'cannot finish timing as no previous time found');
-
-  const result: number = now - previous;
-  const rounded: string = result.toFixed(2);
-
-  const style: Style = (() => {
-    if (result < 16) {
-      return {
-        textColor: 'green',
-        symbol: '✅',
-      };
+  if (process.env.NODE_ENV !== 'production') {
+    if (!isTimingsEnabled()) {
+      return;
     }
-    if (result < 40) {
-      return {
-        textColor: 'orange',
-        symbol: '⚠️',
-      };
-    }
-    return {
-      textColor: 'red',
-      symbol: '❌',
-    };
-  })();
+    const now: number = performance.now();
 
-  // eslint-disable-next-line no-console
-  console.log(
-    `${style.symbol} %cTiming %c${rounded} %cms %c${key}`,
-    // title
-    'color: blue; font-weight: bold; ',
-    // result
-    `color: ${style.textColor}; font-size: 1.1em;`,
-    // ms
-    'color: grey;',
-    // key
-    'color: purple; font-weight: bold;',
-  );
+    const previous: ?number = records[key];
+
+    invariant(previous, 'cannot finish timing as no previous time found');
+
+    const result: number = now - previous;
+    const rounded: string = result.toFixed(2);
+
+    const style: Style = (() => {
+      if (result < 16) {
+        return {
+          textColor: 'green',
+          symbol: '✅',
+        };
+      }
+      if (result < 40) {
+        return {
+          textColor: 'orange',
+          symbol: '⚠️',
+        };
+      }
+      return {
+        textColor: 'red',
+        symbol: '❌',
+      };
+    })();
+
+    // eslint-disable-next-line no-console
+    console.log(
+      `${style.symbol} %cTiming %c${rounded} %cms %c${key}`,
+      // title
+      'color: blue; font-weight: bold; ',
+      // result
+      `color: ${style.textColor}; font-size: 1.1em;`,
+      // ms
+      'color: grey;',
+      // key
+      'color: purple; font-weight: bold;',
+    );
+  }
 };
