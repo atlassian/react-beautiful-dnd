3c4a3f40db383cfb5e9a39e66414fe42442385bc
Alex Reardon <areardon@atlassian.com>
2018-07-02 17:18:46 +1000

    fixing linting

--- src/state/create-store.js
+++ src/state/create-store.js
@@ -9,7 +9,7 @@ import hooks from './middleware/hooks';
 import dropAnimationFinish from './middleware/drop-animation-finish';
 import dimensionMarshalStopper from './middleware/dimension-marshal-stopper';
 import autoScroll from './middleware/auto-scroll';
-import pendingDrop from './middleware/pending-drop';
+// import pendingDrop from './middleware/pending-drop';
 import maxScrollUpdater from './middleware/max-scroll-updater';
 import type { DimensionMarshal } from './dimension-marshal/dimension-marshal-types';
 import type { StyleMarshal } from '../view/style-marshal/style-marshal-types';
