c69ea9e423097b17be5bd94e61cff2473a33fd9d
Alex Reardon <areardon@atlassian.com>
2018-08-20 14:45:45 +1000

    environment

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { configure } from '@storybook/react';
 // adding css reset - storybook includes a css loader
 import '@atlaskit/css-reset';
+import { version } from '../package.json';
 
 // dynamically load in all the stories in the /stories directory
 // https://github.com/storybooks/storybook/issues/125#issuecomment-212404756
@@ -13,4 +14,11 @@ function loadStories() {
 
 configure(loadStories, module);
 
-console.log('Using React version', React.version);
+const table = console.table || console.log;
+
+console.log('environment');
+table([
+  ['react-beautiful-dnd version', version],
+  ['react version', React.version],
+  ['process.env.NODE_ENV', process.env.NODE_ENV],
+]);
