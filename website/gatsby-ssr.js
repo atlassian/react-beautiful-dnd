6771a4908eb5cc151fb00a4d1183de3d64844708
Pavithra Kodmad <pksjce@gmail.com>
2018-08-24 11:58:11 +1000

    Add fav icon (#741)

---
+++ website/gatsby-ssr.js
@@ -0,0 +1,16 @@
+// @flow
+
+type renderBodyParams = { setHeadComponents: (any[]) => mixed };
+
+const React = require('react');
+// $FlowFixMe - Somehow flow doesnt like ico files
+const faviconIco = require('./src/assets/favicon.ico');
+const favicon16 = require('./src/assets/favicon-16x16.png');
+const favicon32 = require('./src/assets/favicon-32x32.png');
+
+exports.onRenderBody = ({ setHeadComponents }: renderBodyParams) =>
+  setHeadComponents([
+    <link rel="shortcut icon" type="image/x-icon" href={faviconIco} />,
+    <link rel="icon" href={favicon16} />,
+    <link rel="icon" href={favicon32} />,
+  ]);
