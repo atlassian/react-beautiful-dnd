149688198ecdb79b63a1e4fd24cd1715cb8804dc
Alex Reardon <areardon@atlassian.com>
2018-08-26 21:39:54 +1000

    tidying up cleanup

--- website/data/SiteConfig.js
+++ website/data/SiteConfig.js
@@ -1,10 +1,11 @@
 // @flow
-const title = 'Beautiful and accessible drag and drop for lists with React';
+const description =
+  'Beautiful and accessible drag and drop for lists with React';
 
 module.exports = {
-  siteTitle: 'react beautiful dnd', // Site title.
-  siteTitleAlt: title, // Alternative site title for SEO.
+  siteTitle: 'react-beautiful-dnd', // Site title.
+  siteTitleAlt: description, // Alternative site title for SEO.
   siteUrl: 'https://react-beautiful-dnd.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: title,
+  siteDescription: description,
 };
