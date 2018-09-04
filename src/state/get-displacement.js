e755f87753ecc65bafbbeb16a0797d5d77377ea3
Alex Reardon <areardon@atlassian.com>
2018-09-04 12:57:12 +1000

    minor cleanup

--- src/state/get-displacement.js
+++ src/state/get-displacement.js
@@ -17,6 +17,23 @@ type Args = {|
   viewport: Rect,
 |};
 
+const getShouldAnimate = (isVisible: boolean, previous: ?Displacement) => {
+  // if should be displaced and not visible
+  if (!isVisible) {
+    return false;
+  }
+
+  // if visible and no previous entries: animate!
+  if (!previous) {
+    return true;
+  }
+
+  // return our previous value
+  // for items that where originally not visible this will be false
+  // otherwise it will be true
+  return previous.shouldAnimate;
+};
+
 // Note: it is also an optimisation to undo the displacement on
 // items when they are no longer visible.
 // This prevents a lot of .render() calls when leaving a list
@@ -37,25 +54,7 @@ export default ({
     viewport,
   });
 
-  const shouldAnimate: boolean = (() => {
-    // if should be displaced and not visible
-    if (!isVisible) {
-      return false;
-    }
-
-    // see if we can find a previous value
-    const previous: ?Displacement = map[id];
-
-    // if visible and no previous entries: animate!
-    if (!previous) {
-      return true;
-    }
-
-    // return our previous value
-    // for items that where originally not visible this will be false
-    // otherwise it will be true
-    return previous.shouldAnimate;
-  })();
+  const shouldAnimate: boolean = getShouldAnimate(isVisible, map[id]);
 
   const displacement: Displacement = {
     draggableId: id,
