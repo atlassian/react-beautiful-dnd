42e5a06b8619dc9f1e08ef922ec272c8fc0e7d01
Alex Reardon <areardon@atlassian.com>
2018-09-05 17:09:34 +1000

    testing global styles correctly

--- test/unit/view/style-marshal/get-styles.spec.js
+++ test/unit/view/style-marshal/get-styles.spec.js
@@ -11,14 +11,20 @@ Object.keys(styles).forEach((key: string) => {
     stylelint
       .lint({
         code: styles[key],
-        configOverrides: {
-          // we merge all the styles into a fairly compressed block
+        config: {
+          // just using the recommended config as it only checks for errors and not formatting
+          extends: ['stylelint-config-recommended'],
+          // basic semi colin rules
           rules: {
-            'no-missing-end-of-source-newline': null,
+            'no-extra-semicolons': true,
+            'declaration-block-semicolon-space-after': 'always-single-line',
           },
         },
       })
       .then(result => {
         expect(result.errored).toBe(false);
+        // asserting that some CSS was actually generated!
+        // eslint-disable-next-line no-underscore-dangle
+        expect(result.results[0]._postcssResult.css.length).toBeGreaterThan(1);
       }));
 });
