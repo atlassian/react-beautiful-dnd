f9f3e59063263ffc5fae34680c25aa4454d58907
Alex Reardon <areardon@atlassian.com>
2018-09-28 17:28:06 +1000

    more tests

--- test/unit/state/middleware/drop/drop-position.spec.js
+++ test/unit/state/middleware/drop/drop-position.spec.js
@@ -15,7 +15,12 @@ import getHomeImpact from '../../../../../src/state/get-home-impact';
 import { origin, subtract } from '../../../../../src/state/position';
 import getDisplacedBy from '../../../../../src/state/get-displaced-by';
 import getDisplacementMap from '../../../../../src/state/get-displacement-map';
-import { goAfter, goBefore } from '../../../../../src/state/move-relative-to';
+import {
+  goAfter,
+  goBefore,
+  goIntoStart,
+} from '../../../../../src/state/move-relative-to';
+import { forward } from '../../../../../src/state/user-direction/user-direction-preset';
 
 const getDisplacement = (draggable: DraggableDimension): Displacement => ({
   isVisible: true,
@@ -152,11 +157,177 @@ const getDisplacement = (draggable: DraggableDimension): Displacement => ({
       expect(result).toEqual(expectedOffset);
     });
 
-    it('should drop after the last item in a list if nothing is displaced', () => {});
+    it('should drop after the last item in a list if nothing is displaced', () => {
+      // inHome1 over the end of foreign
+      const willDisplaceForward: boolean = true;
+      const displaced: Displacement[] = [];
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome1.displaceBy,
+        willDisplaceForward,
+      );
+      const impact: DragImpact = {
+        movement: {
+          displaced,
+          map: getDisplacementMap(displaced),
+          willDisplaceForward,
+          displacedBy,
+        },
+        direction: axis.direction,
+        // moving into the last position
+        destination: {
+          index: preset.inForeignList.length - 1,
+          droppableId: preset.foreign.descriptor.id,
+        },
+        merge: null,
+      };
 
-    it('should drop into the center of an item that is being combined with', () => {});
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: preset.dimensions,
+        viewport: preset.viewport,
+      });
 
-    it('should drop into the center of a displaced combined item', () => {});
+      const expectedCenter: Position = goAfter({
+        axis,
+        moveRelativeTo: preset.inForeign4.client,
+        isMoving: preset.inHome1.client,
+      });
+      const original: Position = preset.inHome1.client.borderBox.center;
+      const expectedOffset: Position = subtract(expectedCenter, original);
+      expect(result).toEqual(expectedOffset);
+    });
+
+    it('should drop into the start of an empty list', () => {
+      // inHome1 over the end of empty
+      const willDisplaceForward: boolean = true;
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome1.displaceBy,
+        willDisplaceForward,
+      );
+      const impact: DragImpact = {
+        movement: {
+          displaced: [],
+          map: {},
+          willDisplaceForward,
+          displacedBy,
+        },
+        direction: axis.direction,
+        // moving into the last position
+        destination: {
+          index: 0,
+          droppableId: preset.emptyForeign.descriptor.id,
+        },
+        merge: null,
+      };
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: preset.dimensions,
+        viewport: preset.viewport,
+      });
+
+      const expectedCenter: Position = goIntoStart({
+        axis,
+        moveInto: preset.emptyForeign.client,
+        isMoving: preset.inHome1.client,
+      });
+      const original: Position = preset.inHome1.client.borderBox.center;
+      const expectedOffset: Position = subtract(expectedCenter, original);
+      expect(result).toEqual(expectedOffset);
+    });
+
+    it('should drop into the center of an item that is being combined with', () => {
+      // inHome1 combining with inHome2
+      const willDisplaceForward: boolean = true;
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome1.displaceBy,
+        willDisplaceForward,
+      );
+      const impact: DragImpact = {
+        movement: {
+          displaced: [],
+          map: {},
+          willDisplaceForward,
+          displacedBy,
+        },
+        direction: axis.direction,
+        destination: null,
+        merge: {
+          whenEntered: forward,
+          combine: {
+            draggableId: preset.inHome2.descriptor.id,
+            droppableId: preset.home.descriptor.id,
+          },
+        },
+      };
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: preset.dimensions,
+        viewport: preset.viewport,
+      });
+
+      const expectedCenter: Position = preset.inHome2.client.borderBox.center;
+      const original: Position = preset.inHome1.client.borderBox.center;
+      const expectedOffset: Position = subtract(expectedCenter, original);
+      expect(result).toEqual(expectedOffset);
+    });
+
+    it('should drop into the center of a displaced combined item', () => {
+      // inHome1 combining with displaced inForeign1
+      // displacing forward in foreign list
+      const willDisplaceForward: boolean = true;
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome1.displaceBy,
+        willDisplaceForward,
+      );
+      // displaced inForeign1 forward
+      const displaced: Displacement[] = [getDisplacement(preset.inForeign1)];
+      const impact: DragImpact = {
+        movement: {
+          displaced,
+          map: getDisplacementMap(displaced),
+          willDisplaceForward,
+          displacedBy,
+        },
+        direction: axis.direction,
+        destination: null,
+        merge: {
+          whenEntered: forward,
+          combine: {
+            draggableId: preset.inForeign1.descriptor.id,
+            droppableId: preset.home.descriptor.id,
+          },
+        },
+      };
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: preset.dimensions,
+        viewport: preset.viewport,
+      });
+
+      const displacedInForeign1: BoxModel = offset(
+        preset.inForeign1.client,
+        displacedBy.point,
+      );
+      const expectedCenter: Position = displacedInForeign1.borderBox.center;
+      const original: Position = preset.inHome1.client.borderBox.center;
+      const expectedOffset: Position = subtract(expectedCenter, original);
+      expect(result).toEqual(expectedOffset);
+    });
 
     it('should account for the scroll of the droppable you are over when reordering', () => {});
 
