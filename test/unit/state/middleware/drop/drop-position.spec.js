ddbd4cace13f4329a379125a6b20804ec33864e7
Alex Reardon <areardon@atlassian.com>
2018-10-02 09:02:03 +1000

    accounting for scroll changes

--- test/unit/state/middleware/drop/drop-position.spec.js
+++ test/unit/state/middleware/drop/drop-position.spec.js
@@ -1,18 +1,26 @@
 // @flow
 import { offset, type Position, type BoxModel } from 'css-box-model';
 import { vertical, horizontal } from '../../../../../src/state/axis';
-import { getPreset } from '../../../../utils/dimension';
+import { getPreset, makeScrollable } from '../../../../utils/dimension';
 import type {
   Axis,
   DisplacedBy,
   Displacement,
   DragImpact,
   DraggableDimension,
+  DroppableDimension,
+  DimensionMap,
+  Viewport,
 } from '../../../../../src/types';
 import getOffsetFromStart from '../../../../../src/state/middleware/drop/get-new-home-client-offset';
 import noImpact from '../../../../../src/state/no-impact';
 import getHomeImpact from '../../../../../src/state/get-home-impact';
-import { origin, subtract } from '../../../../../src/state/position';
+import {
+  origin,
+  subtract,
+  negate,
+  add,
+} from '../../../../../src/state/position';
 import getDisplacedBy from '../../../../../src/state/get-displaced-by';
 import getDisplacementMap from '../../../../../src/state/get-displacement-map';
 import {
@@ -24,6 +32,8 @@ import {
   forward,
   backward,
 } from '../../../../../src/state/user-direction/user-direction-preset';
+import scrollDroppable from '../../../../../src/state/droppable/scroll-droppable';
+import scrollViewport from '../../../../../src/state/scroll-viewport';
 
 const getDisplacement = (draggable: DraggableDimension): Displacement => ({
   isVisible: true,
@@ -380,12 +390,134 @@ const getDisplacement = (draggable: DraggableDimension): Displacement => ({
       expect(result).toEqual(expectedOffset);
     });
 
-    it('should account for the scroll of the droppable you are over when reordering', () => {});
+    it('should account for the scroll of the droppable you are over when reordering', () => {
+      const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+      const scroll: Position = { x: 10, y: 15 };
+      const displacement: Position = negate(scroll);
+      const scrolled: DroppableDimension = scrollDroppable(
+        scrollableHome,
+        scroll,
+      );
+      const withScrolled: DimensionMap = {
+        ...preset.dimensions,
+        droppables: {
+          ...preset.droppables,
+          [scrolled.descriptor.id]: scrolled,
+        },
+      };
+      const impact: DragImpact = getHomeImpact(preset.inHome1, preset.home);
 
-    it('should account for the scroll of the droppable you are over when combining', () => {});
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: withScrolled,
+        viewport: preset.viewport,
+      });
 
-    it('should account for the scroll of your home list if you are not over any list', () => {});
+      expect(result).toEqual(displacement);
+    });
 
-    it('should account for any changes in the window scroll', () => {});
+    it('should account for the scroll of the droppable you are over when combining', () => {
+      const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+      const scroll: Position = { x: 10, y: 15 };
+      const displacement: Position = negate(scroll);
+      const scrolled: DroppableDimension = scrollDroppable(
+        scrollableHome,
+        scroll,
+      );
+      const withScrolled: DimensionMap = {
+        ...preset.dimensions,
+        droppables: {
+          ...preset.droppables,
+          [scrolled.descriptor.id]: scrolled,
+        },
+      };
+      // inHome1 combining with inHome2
+      const willDisplaceForward: boolean = false;
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome1.displaceBy,
+        willDisplaceForward,
+      );
+      const impact: DragImpact = {
+        movement: {
+          displaced: [],
+          map: {},
+          willDisplaceForward,
+          displacedBy,
+        },
+        direction: axis.direction,
+        destination: null,
+        merge: {
+          whenEntered: forward,
+          combine: {
+            draggableId: preset.inHome2.descriptor.id,
+            droppableId: preset.home.descriptor.id,
+          },
+        },
+      };
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact,
+        draggable: preset.inHome1,
+        dimensions: withScrolled,
+        viewport: preset.viewport,
+      });
+
+      const expectedCenter: Position = preset.inHome2.client.borderBox.center;
+      const original: Position = preset.inHome1.client.borderBox.center;
+      const centerDiff: Position = subtract(expectedCenter, original);
+      const expectedOffset: Position = add(centerDiff, displacement);
+      expect(result).toEqual(expectedOffset);
+    });
+
+    it('should account for the scroll of your home list if you are not over any list', () => {
+      const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+      const scroll: Position = { x: 10, y: 15 };
+      const displacement: Position = negate(scroll);
+      const scrolled: DroppableDimension = scrollDroppable(
+        scrollableHome,
+        scroll,
+      );
+      const withScrolled: DimensionMap = {
+        ...preset.dimensions,
+        droppables: {
+          ...preset.droppables,
+          [scrolled.descriptor.id]: scrolled,
+        },
+      };
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact: noImpact,
+        draggable: preset.inHome1,
+        dimensions: withScrolled,
+        viewport: preset.viewport,
+      });
+
+      expect(result).toEqual(displacement);
+    });
+
+    it('should account for any changes in the window scroll', () => {
+      const scroll: Position = { x: 10, y: 15 };
+      const displacement: Position = negate(scroll);
+      const scrolled: Viewport = scrollViewport(
+        preset.viewport,
+        // adding to the existing scroll
+        add(preset.windowScroll, scroll),
+      );
+
+      const result: Position = getOffsetFromStart({
+        reason: 'DROP',
+        impact: noImpact,
+        draggable: preset.inHome1,
+        dimensions: preset.dimensions,
+        viewport: scrolled,
+      });
+
+      expect(result).toEqual(displacement);
+    });
   });
 });
