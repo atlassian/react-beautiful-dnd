cbc609fb06f3282139fa98eb4bce8179b1e39042
Alex Reardon <areardon@atlassian.com>
2018-10-03 09:15:51 +1000

    simplier regex

--- src/debug/dev-warning.js
+++ src/debug/dev-warning.js
@@ -1,10 +1,10 @@
 // @flow
 
-// not replacing newlines
-const spaceAndTab: RegExp = /[ \t]{2,}/gm;
+// not replacing newlines (which \s does)
+const spacesAndTabs: RegExp = /[ \t]{2,}/g;
 
-// using .trim() to clear the any newlines before the first text
-const clean = (value: string) => value.replace(spaceAndTab, ' ').trim();
+// using .trim() to clear the any newlines before the first text and after last text
+const clean = (value: string) => value.replace(spacesAndTabs, ' ').trim();
 
 export const getDevMessage = (message: string) =>
   clean(`
