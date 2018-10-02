cb9d3e235757020050508d026ba29761adaa8d4d
Alex Reardon <areardon@atlassian.com>
2018-10-02 10:21:40 +1000

    adding more file path tests

--- test/unit/integration/src-file-name-convention.spec.js
+++ test/unit/integration/src-file-name-convention.spec.js
@@ -2,18 +2,26 @@
 import globby from 'globby';
 import invariant from 'tiny-invariant';
 
-const convention: RegExp = /^[a-z0-9\-\.\/]+$/;
+// Regex playground: https://regexr.com/40fin
+const convention: RegExp = /^[a-z0-9\-_./]+$/;
 const isSnakeCase = (filePath: string): boolean => convention.test(filePath);
 
 it('should have every source file following the file name convention', async () => {
-  console.log('running');
   const src = await globby('src/**/*');
-  const website = await globby('website/src/**/*');
+  const docs = await globby('docs/**/*');
+  const test = await globby('test/**/*');
+  const website = await globby([
+    'website/src/**/*',
+    // TODO: fix
+    // 'website/documentation/**/*',
+  ]);
 
   invariant(src.length, 'Could not find /src files');
+  invariant(docs.length, 'Could not find /docs files');
+  invariant(test.length, 'Could not find /test/src files');
   invariant(website.length, 'Could not find /website/src files');
 
-  [...src, ...website].forEach((filePath: string) => {
+  [...src, ...test, ...website, ...docs].forEach((filePath: string) => {
     const isMatching: boolean = isSnakeCase(filePath);
 
     if (!isMatching) {
