af423939250f9cd3fc885094d534a5a61d9efd32
Alex Reardon <areardon@atlassian.com>
2018-10-24 08:40:36 +1100

    initial

--- test/unit/state/move-in-direction/move-to-next-place/move-to-next-index/from-reorder/no-moving-outside-list.spec.js
+++
@@ -1,8 +0,0 @@
-// @flow
-it('should not allow movement beyond the last item in a home list', () => {});
-
-it('should not allow movement beyond the last item in a foreign list', () => {});
-
-it('should not allow movement before the first item in a home list', () => {});
-
-it('should not allow movement before the first item in a foreign list', () => {});
