e6ad7d7f8e6bafcfbc967374c38b7f687bff978e
Alex Reardon <areardon@atlassian.com>
2018-10-24 15:59:13 +1100

    tests for moving from combining with displaced items

--- test/unit/state/move-in-direction/move-to-next-place/move-to-next-index/from-combine/from-combine-with-non-displaced.spec.js
+++ test/unit/state/move-in-direction/move-to-next-place/move-to-next-index/from-combine/from-combine-with-non-displaced.spec.js
@@ -6,6 +6,7 @@ import type {
   Displacement,
   DisplacedBy,
   DroppableDimension,
+  DraggableDimension,
 } from '../../../../../../../src/types';
 import { vertical, horizontal } from '../../../../../../../src/state/axis';
 import {
@@ -22,11 +23,17 @@ const enableCombine = (droppable: DroppableDimension): DroppableDimension => ({
   isCombineEnabled: true,
 });
 
+const visibleDisplacement = (draggable: DraggableDimension): Displacement => ({
+  draggableId: draggable.descriptor.id,
+  isVisible: true,
+  shouldAnimate: true,
+});
+
 [vertical, horizontal].forEach((axis: Axis) => {
   const preset = getPreset(axis);
   describe(`on ${axis.direction} axis`, () => {
     describe('in area that would usually displace backwards', () => {
-      // inHome1 is combined with a non-displaced inHome2
+      // inHome1 has moved past inHome2 and is now combined with a non-displaced inHome3
       const willDisplaceForward: boolean = false;
       const displacedBy: DisplacedBy = getDisplacedBy(
         axis,
@@ -34,17 +41,18 @@ const enableCombine = (droppable: DroppableDimension): DroppableDimension => ({
         willDisplaceForward,
       );
 
-      const whenCombined: DragImpact = {
+      const initial: Displacement[] = [visibleDisplacement(preset.inHome2)];
+      const combinedWithInHome3: DragImpact = {
         movement: {
-          displaced: [],
-          map: {},
+          displaced: initial,
+          map: getDisplacementMap(initial),
           displacedBy,
           willDisplaceForward,
         },
         merge: {
           whenEntered: forward,
           combine: {
-            draggableId: preset.inHome2.descriptor.id,
+            draggableId: preset.inHome3.descriptor.id,
             droppableId: preset.home.descriptor.id,
           },
         },
@@ -61,16 +69,113 @@ const enableCombine = (droppable: DroppableDimension): DroppableDimension => ({
             draggables: preset.draggables,
             destination: enableCombine(preset.home),
             insideDestination: preset.inHomeList,
-            previousImpact: whenCombined,
+            previousImpact: combinedWithInHome3,
+          });
+          invariant(impact);
+
+          // ordered by closest
+          const displaced: Displacement[] = [
+            visibleDisplacement(preset.inHome3),
+            visibleDisplacement(preset.inHome2),
+          ];
+          const expected: DragImpact = {
+            movement: {
+              displaced,
+              map: getDisplacementMap(displaced),
+              displacedBy,
+              willDisplaceForward,
+            },
+            merge: null,
+            direction: preset.home.axis.direction,
+            destination: {
+              droppableId: preset.home.descriptor.id,
+              index: preset.inHome3.descriptor.index,
+            },
+          };
+          expect(impact).toEqual(expected);
+        });
+      });
+
+      describe('is moving backwards (will decrease displacement)', () => {
+        it('should move into the spot behind the combined item and not displace it', () => {
+          // moving back behind inHome3
+          const impact: ?DragImpact = moveToNextIndex({
+            isMovingForward: false,
+            isInHomeList: true,
+            draggable: preset.inHome1,
+            draggables: preset.draggables,
+            destination: enableCombine(preset.home),
+            insideDestination: preset.inHomeList,
+            previousImpact: combinedWithInHome3,
           });
           invariant(impact);
 
           const displaced: Displacement[] = [
-            {
-              draggableId: preset.inHome2.descriptor.id,
-              isVisible: true,
-              shouldAnimate: true,
+            visibleDisplacement(preset.inHome2),
+          ];
+          const expected: DragImpact = {
+            movement: {
+              displaced,
+              map: getDisplacementMap(displaced),
+              displacedBy,
+              willDisplaceForward,
             },
+            merge: null,
+            direction: preset.home.axis.direction,
+            destination: {
+              droppableId: preset.home.descriptor.id,
+              index: preset.inHome2.descriptor.index,
+            },
+          };
+          expect(impact).toEqual(expected);
+        });
+      });
+    });
+
+    describe('in area that would usually displace forwards', () => {
+      // inHome3 has moved backwards past inHome2 and is now combined with a non-displaced inHome1
+      const willDisplaceForward: boolean = true;
+      const displacedBy: DisplacedBy = getDisplacedBy(
+        axis,
+        preset.inHome3.displaceBy,
+        willDisplaceForward,
+      );
+
+      const initial: Displacement[] = [visibleDisplacement(preset.inHome2)];
+      const combinedWithInHome1: DragImpact = {
+        movement: {
+          displaced: initial,
+          map: getDisplacementMap(initial),
+          displacedBy,
+          willDisplaceForward,
+        },
+        merge: {
+          whenEntered: backward,
+          combine: {
+            draggableId: preset.inHome1.descriptor.id,
+            droppableId: preset.home.descriptor.id,
+          },
+        },
+        direction: preset.home.axis.direction,
+        destination: null,
+      };
+
+      describe('is moving forwards (will decrease displacement)', () => {
+        it('should move into the spot in front of the combined item and leave it in place', () => {
+          const impact: ?DragImpact = moveToNextIndex({
+            isMovingForward: true,
+            isInHomeList: true,
+            draggable: preset.inHome3,
+            draggables: preset.draggables,
+            destination: enableCombine(preset.home),
+            insideDestination: preset.inHomeList,
+            previousImpact: combinedWithInHome1,
+          });
+          invariant(impact);
+
+          // ordered by closest
+          const displaced: Displacement[] = [
+            visibleDisplacement(preset.inHome2),
           ];
           const expected: DragImpact = {
             movement: {
@@ -90,23 +195,28 @@ const enableCombine = (droppable: DroppableDimension): DroppableDimension => ({
         });
       });
 
-      describe('is moving backwards (will decrease displacement)', () => {
-        it('should move into the spot behind the combined item and not displace it', () => {
+      describe('is moving backwards (will increase displacement)', () => {
+        it('should move into the spot of the combined item and push it backwards', () => {
+          // moving inHome3 forwards after combining with inHome1
           const impact: ?DragImpact = moveToNextIndex({
             isMovingForward: false,
             isInHomeList: true,
-            draggable: preset.inHome1,
+            draggable: preset.inHome3,
             draggables: preset.draggables,
             destination: enableCombine(preset.home),
             insideDestination: preset.inHomeList,
-            previousImpact: whenCombined,
+            previousImpact: combinedWithInHome1,
           });
           invariant(impact);
 
+          const displaced: Displacement[] = [
+            visibleDisplacement(preset.inHome1),
+            visibleDisplacement(preset.inHome2),
+          ];
           const expected: DragImpact = {
             movement: {
-              displaced: [],
-              map: {},
+              displaced,
+              map: getDisplacementMap(displaced),
               displacedBy,
               willDisplaceForward,
             },
@@ -121,6 +231,5 @@ const enableCombine = (droppable: DroppableDimension): DroppableDimension => ({
         });
       });
     });
-    describe('in area that would usually displace forwards', () => {});
   });
 });
