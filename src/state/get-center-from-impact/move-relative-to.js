26b722de6ebb1851af71391613d8b2445a2bb7bd
Alex Reardon <areardon@atlassian.com>
2018-11-14 14:07:00 +1100

    new approach

--- src/state/get-center-from-impact/move-relative-to.js
+++ src/state/get-center-from-impact/move-relative-to.js
@@ -10,29 +10,22 @@ type Args = {|
 |};
 
 const distanceFromStartToCenter = (axis: Axis, box: BoxModel): number =>
-  box.margin[axis.start] +
-  box.border[axis.start] +
-  box.padding[axis.start] +
-  box.contentBox[axis.size] / 2;
+  box.margin[axis.start] + box.borderBox[axis.size] / 2;
 
 const distanceFromEndToCenter = (axis: Axis, box: BoxModel): number =>
-  box.margin[axis.end] +
-  box.border[axis.end] +
-  box.padding[axis.end] +
-  box.contentBox[axis.size] / 2;
+  box.margin[axis.end] + box.borderBox[axis.size] / 2;
 
 // We align the moving item against the cross axis start of the target
 // We used to align the moving item cross axis center with the cross axis center of the target.
 // However, this leads to a bad experience when reordering columns
-// We use the marginBox crossAxisSize of the moving item rather than
-// building up a `distanceFromCrossAxisStart` as it might have unbalanced spacing
-// (such as bigger padding on one side then the other)
 const getCrossAxisCenter = (
   axis: Axis,
   target: Rect,
   isMoving: BoxModel,
 ): number =>
-  target[axis.crossAxisStart] + isMoving.marginBox[axis.crossAxisSize] / 2;
+  target[axis.crossAxisStart] +
+  isMoving.margin[axis.crossAxisStart] +
+  isMoving.borderBox[axis.crossAxisSize] / 2;
 
 export const goAfter = ({ axis, moveRelativeTo, isMoving }: Args): Position =>
   patch(
