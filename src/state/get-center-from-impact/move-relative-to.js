10b11a3b677380e716bd5b26420f19646a1e62a4
Alex Reardon <areardon@atlassian.com>
2018-11-14 11:43:39 +1100

    adding more comments

--- src/state/get-center-from-impact/move-relative-to.js
+++ src/state/get-center-from-impact/move-relative-to.js
@@ -24,6 +24,9 @@ const distanceFromEndToCenter = (axis: Axis, box: BoxModel): number =>
 // We align the moving item against the cross axis start of the target
 // We used to align the moving item cross axis center with the cross axis center of the target.
 // However, this leads to a bad experience when reordering columns
+// We use the marginBox crossAxisSize of the moving item rather than
+// building up a `distanceFromCrossAxisStart` as it might have unbalanced spacing
+// (such as bigger padding on one side then the other)
 const getCrossAxisCenter = (
   axis: Axis,
   target: Rect,
