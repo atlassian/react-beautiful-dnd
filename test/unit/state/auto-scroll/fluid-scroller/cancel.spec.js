160f2aada813a8e02843b26bcd4b6f3e0a344004
Alex Reardon <areardon@atlassian.com>
2018-12-04 07:40:24 +1100

    moving code around

--- test/unit/state/auto-scroll/fluid-scroller/cancel.spec.js
+++ test/unit/state/auto-scroll/fluid-scroller/cancel.spec.js
@@ -1,75 +1,17 @@
 // @flow
-import type { Position } from 'css-box-model';
 import forEach, { type BlockFnArgs } from './util/for-each';
 import type { DraggingState } from '../../../../../src/types';
-import { scrollableViewport } from './util/viewport';
-import dragTo from './util/drag-to';
-import {
-  getPixelThresholds,
-  type PixelThresholds,
+import getMocks from './util/get-args-mock';
+import getScroller, {
+  type FluidScroller,
 } from '../../../../../src/state/auto-scroller/fluid-scroller';
-import getDroppable from './util/get-droppable';
-import { patch } from '../../../../../src/state/position';
-import { addDroppable } from '../../../../utils/dimension';
 
-forEach(({ axis, scroller, state, mocks, preset }: BlockFnArgs) => {
+forEach(({ axis, scroller, state, preset }: BlockFnArgs) => {
   it('should cancel any pending window scroll', () => {
-    const thresholds: PixelThresholds = getPixelThresholds(
-      scrollableViewport.frame,
-      axis,
-    );
-    const onMaxBoundary: Position = patch(
-      axis.line,
-      scrollableViewport.frame[axis.size] - thresholds.maxSpeedAt,
-      scrollableViewport.frame.center[axis.crossAxisLine],
-    );
-
-    scroller(
-      dragTo({
-        state,
-        selection: onMaxBoundary,
-        viewport: scrollableViewport,
-      }),
-    );
-
-    // frame not cleared
-    expect(mocks.scrollWindow).not.toHaveBeenCalled();
-
-    scroller.cancel();
-    requestAnimationFrame.flush();
-
-    expect(mocks.scrollWindow).not.toHaveBeenCalled();
+    throw new Error('TODO');
   });
 
   it('should cancel any pending droppable scroll', () => {
-    const { scrollable, frameClient } = getDroppable(preset);
-    const thresholds: PixelThresholds = getPixelThresholds(
-      frameClient.borderBox,
-      axis,
-    );
-    const onMaxBoundary: Position = patch(
-      axis.line,
-      frameClient.borderBox[axis.size] - thresholds.maxSpeedAt,
-      frameClient.borderBox.center[axis.crossAxisLine],
-    );
-    const drag: DraggingState = addDroppable(
-      dragTo({
-        state,
-        selection: onMaxBoundary,
-        viewport: scrollableViewport,
-      }),
-      scrollable,
-    );
-
-    scroller(drag);
-
-    // frame not cleared
-    expect(mocks.scrollDroppable).not.toHaveBeenCalled();
-
-    // should cancel the next frame
-    scroller.cancel();
-    requestAnimationFrame.flush();
-
-    expect(mocks.scrollDroppable).not.toHaveBeenCalled();
+    throw new Error('TODO');
   });
 });
--- test/unit/state/auto-scroll/fluid-scroller/lifecycle.spec.js
+++ test/unit/state/auto-scroll/fluid-scroller/lifecycle.spec.js
@@ -1,14 +1,15 @@
 // @flow
-
 import forEach, { type BlockFnArgs } from './util/for-each';
 import type { DraggingState } from '../../../../../src/types';
-import getScroller from './util/get-scroller';
-import { type FluidScroller } from '../../../../../src/state/auto-scroller/fluid-scroller';
+import getMocks from './util/get-args-mock';
+import getScroller, {
+  type FluidScroller,
+} from '../../../../../src/state/auto-scroller/fluid-scroller';
 
 forEach(({ state }: BlockFnArgs) => {
   const base: DraggingState = state.dragging();
   it('should throw if a scroll occurs before a drag as started', () => {
-    const scroller: FluidScroller = getScroller();
+    const scroller: FluidScroller = getScroller(getMocks());
     expect(() => scroller.scroll(base)).toThrow();
 
     // after a drag
@@ -19,7 +20,7 @@ forEach(({ state }: BlockFnArgs) => {
   });
 
   it('should allow subsequent drags', () => {
-    const scroller: FluidScroller = getScroller();
+    const scroller: FluidScroller = getScroller(getMocks());
     const run = () => {
       scroller.start(base);
       scroller.stop();
@@ -31,7 +32,7 @@ forEach(({ state }: BlockFnArgs) => {
   });
 
   it('should allow defensive stop calls', () => {
-    const scroller: FluidScroller = getScroller();
+    const scroller: FluidScroller = getScroller(getMocks());
     // newly created - not started
     scroller.stop();
 
@@ -43,12 +44,8 @@ forEach(({ state }: BlockFnArgs) => {
   });
 
   it('should throw if started multiple times', () => {
-    const scroller: FluidScroller = getScroller();
+    const scroller: FluidScroller = getScroller(getMocks());
     scroller.start(base);
     expect(() => scroller.start(base)).toThrow();
-  });
-
-  it('should cancel any pending scrolls when stopped', () => {
-    throw new Error('TODO');
   });
 });
