a7438c149cfa48bab68616d5269fb05a6337b661
Alex Reardon <alexreardon@users.noreply.github.com>
2019-01-29 14:44:45 +1100

    goodbye for now (#1076)

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,7 @@
 module.exports = {
   setupFiles: ['./test/setup.js'],
   // node_modules is default.
-  // We want to ignore any tests in the website(sometimes they are added to / website /.cache)
-  testPathIgnorePatterns: ['/node_modules/', '/website/.cache/'],
+  testPathIgnorePatterns: ['/node_modules/'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
