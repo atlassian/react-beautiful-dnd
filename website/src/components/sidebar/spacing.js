694999e1916d120a00143dfc42a49ebbfe070164
Alex Reardon <areardon@atlassian.com>
2019-01-29 14:29:07 +1100

    goodbye for now

--- website/src/components/sidebar/spacing.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import { grid, gutter } from '../../constants';
-
-const spacing = {
-  top: grid * 3,
-  side: gutter.normal,
-};
-
-export default spacing;
--- website/src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-// @flow
-import Landing from '../components/landing';
-
-export default Landing;
-
-// Gatsby was complaining about this:
-// export { default } from '../components/landing';
