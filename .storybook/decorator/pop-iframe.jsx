06a0e8a980bf09eb6b76974c1fb2c8819d38e9f0
Alex Reardon <areardon@atlassian.com>
2019-01-30 14:44:18 +1100

    fixing naming

--- .storybook/decorator/pop-iframe.jsx
+++ .storybook/decorator/pop-iframe.jsx
@@ -20,7 +20,8 @@ const Button = styled.button`
 `;
 
 const isSSR: boolean = typeof window === 'undefined';
-const isInIframe: boolean = (() => {
+
+const canPopOutOfIframe: boolean = (() => {
   if (isSSR) {
     return false;
   }
@@ -28,12 +29,23 @@ const isInIframe: boolean = (() => {
     // this can violate a same origin policy if on a different domain
     return window.self !== window.top;
   } catch (e) {
-    return true;
+    // cannot pop out as it would violate the same origin policy
+    return false;
   }
 })();
 
-const canPopOutOfIframe: boolean = !isSSR && isInIframe;
-const canPopIntoIframe: boolean = !isSSR && !isInIframe;
+const canPopIntoIframe: boolean = (() => {
+  if (isSSR || canPopOutOfIframe) {
+    return false;
+  }
+  // already the top level
+  try {
+    return window.self === window.top;
+  } catch (e) {
+    // would have been in an iframe that we cannot leave - this codepath should never be hit
+    return false;
+  }
+})();
 
 class PopIframe extends React.Component<Props> {
   getButton = () => {
