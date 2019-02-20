98872762298adba2b3156105a0b51b95e7474cd9
Alex Reardon <areardon@atlassian.com>
2019-02-21 09:49:38 +1100

    pulling tests out again

--- test/unit/state/get-center-from-impact/get-client-border-box-center.spec.js
+++ test/unit/state/get-center-from-impact/get-client-border-box-center.spec.js
@@ -1,163 +1,49 @@
 // @flow
 import type { Position } from 'css-box-model';
-import type {
-  DroppableDimension,
-  Viewport,
-  Axis,
-  DragImpact,
-  DisplacedBy,
-  Displacement,
-} from '../../../../src/types';
-import { vertical, horizontal } from '../../../../src/state/axis';
-import { add, negate, subtract } from '../../../../src/state/position';
-import scrollDroppable from '../../../../src/state/droppable/scroll-droppable';
-import { getPreset, makeScrollable } from '../../../utils/dimension';
-import getClientBorderBoxCenter from '../../../../src/state/get-center-from-impact/get-client-border-box-center';
-import getDisplacedBy from '../../../../src/state/get-displaced-by';
-import { forward } from '../../../../src/state/user-direction/user-direction-preset';
-import noImpact from '../../../../src/state/no-impact';
+import type { DraggableDimension, Viewport } from '../../../../src/types';
+import { add, subtract } from '../../../../src/state/position';
+import { getPreset } from '../../../utils/dimension';
 import scrollViewport from '../../../../src/state/scroll-viewport';
+import getClientBorderBoxCenter from '../../../../src/state/get-center-from-impact/get-client-border-box-center';
 import getHomeOnLift from '../../../../src/state/get-home-on-lift';
-import getDisplacementMap from '../../../../src/state/get-displacement-map';
-import getNotAnimatedDisplacement from '../../../utils/get-displacement/get-not-animated-displacement';
 
-[vertical, horizontal].forEach((axis: Axis) => {
-  const preset = getPreset();
-  const { onLift, impact: homeImpact } = getHomeOnLift({
-    draggable: preset.inHome1,
-    draggables: preset.draggables,
-    home: preset.home,
+const preset = getPreset();
+
+const draggable: DraggableDimension = preset.inHome1;
+const originalClientCenter: Position = preset.inHome1.client.borderBox.center;
+const { onLift, impact } = getHomeOnLift({
+  draggable,
+  home: preset.home,
+  draggables: preset.draggables,
+  viewport: preset.viewport,
+});
+
+it('should give the client center without scroll change', () => {
+  const result: Position = getClientBorderBoxCenter({
+    impact,
+    draggable,
+    droppable: preset.home,
+    draggables: preset.dimensions.draggables,
     viewport: preset.viewport,
+    onLift,
   });
 
-  it('should account for the scroll of the droppable you are over when reordering', () => {
-    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
-    const scroll: Position = { x: 10, y: 15 };
-    const displacement: Position = negate(scroll);
-    const scrolled: DroppableDimension = scrollDroppable(
-      scrollableHome,
-      scroll,
-    );
+  expect(result).toEqual(originalClientCenter);
+});
 
-    const newClientCenter: Position = getClientBorderBoxCenter({
-      impact: homeImpact,
-      draggable: preset.inHome1,
-      droppable: scrolled,
-      draggables: preset.draggables,
-      viewport: preset.viewport,
-      onLift,
-    });
-    const offset: Position = subtract(
-      newClientCenter,
-      preset.inHome1.client.borderBox.center,
-    );
+it('should unwind any changes in viewport scroll', () => {
+  const scroll: Position = { x: 10, y: 20 };
+  const newScroll: Position = add(preset.windowScroll, scroll);
+  const scrolled: Viewport = scrollViewport(preset.viewport, newScroll);
 
-    expect(offset).toEqual(displacement);
+  const result: Position = getClientBorderBoxCenter({
+    impact,
+    draggable,
+    droppable: preset.home,
+    draggables: preset.dimensions.draggables,
+    viewport: scrolled,
+    onLift,
   });
 
-  it('should account for the scroll of the droppable you are over when combining', () => {
-    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
-    const scroll: Position = { x: 10, y: 15 };
-    const displacement: Position = negate(scroll);
-    const scrolled: DroppableDimension = scrollDroppable(
-      scrollableHome,
-      scroll,
-    );
-    // inHome1 combining with inHome2
-    const displacedBy: DisplacedBy = getDisplacedBy(
-      axis,
-      preset.inHome1.displaceBy,
-    );
-    const displaced: Displacement[] = [
-      getNotAnimatedDisplacement(preset.inHome2),
-      getNotAnimatedDisplacement(preset.inHome3),
-      getNotAnimatedDisplacement(preset.inHome4),
-    ];
-    const impact: DragImpact = {
-      movement: {
-        displaced,
-        map: getDisplacementMap(displaced),
-        displacedBy,
-      },
-      direction: axis.direction,
-      destination: null,
-      merge: {
-        whenEntered: forward,
-        combine: {
-          draggableId: preset.inHome2.descriptor.id,
-          droppableId: preset.home.descriptor.id,
-        },
-      },
-    };
-
-    const newClientCenter: Position = getClientBorderBoxCenter({
-      impact,
-      draggable: preset.inHome1,
-      draggables: preset.draggables,
-      droppable: scrolled,
-      viewport: preset.viewport,
-      onLift,
-    });
-    const offset: Position = subtract(
-      newClientCenter,
-      preset.inHome1.client.borderBox.center,
-    );
-
-    const expectedCenter: Position = preset.inHome2.client.borderBox.center;
-    const original: Position = preset.inHome1.client.borderBox.center;
-    const centerDiff: Position = subtract(expectedCenter, original);
-    const expectedOffset: Position = add(centerDiff, displacement);
-    expect(offset).toEqual(expectedOffset);
-  });
-
-  it('should account for the scroll of your home list if you are not over any list', () => {
-    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
-    const scroll: Position = { x: 10, y: 15 };
-    const displacement: Position = negate(scroll);
-    const scrolled: DroppableDimension = scrollDroppable(
-      scrollableHome,
-      scroll,
-    );
-
-    const newClientCenter: Position = getClientBorderBoxCenter({
-      // over nothing
-      impact: noImpact,
-      draggable: preset.inHome1,
-      draggables: preset.draggables,
-      droppable: scrolled,
-      viewport: preset.viewport,
-      onLift,
-    });
-    const offset: Position = subtract(
-      newClientCenter,
-      preset.inHome1.client.borderBox.center,
-    );
-
-    expect(offset).toEqual(displacement);
-  });
-
-  it('should account for any changes in the window scroll', () => {
-    const scroll: Position = { x: 10, y: 15 };
-    const displacement: Position = negate(scroll);
-    const scrolled: Viewport = scrollViewport(
-      preset.viewport,
-      // adding to the existing scroll
-      add(preset.windowScroll, scroll),
-    );
-
-    const newClientCenter: Position = getClientBorderBoxCenter({
-      impact: noImpact,
-      draggable: preset.inHome1,
-      draggables: preset.draggables,
-      droppable: preset.home,
-      viewport: scrolled,
-      onLift,
-    });
-    const offset: Position = subtract(
-      newClientCenter,
-      preset.inHome1.client.borderBox.center,
-    );
-
-    expect(offset).toEqual(displacement);
-  });
+  expect(result).toEqual(subtract(originalClientCenter, scroll));
 });
---
+++ test/unit/state/middleware/drop/drop-position.spec.js
@@ -0,0 +1,163 @@
+// @flow
+import type { Position } from 'css-box-model';
+import type {
+  DroppableDimension,
+  Viewport,
+  Axis,
+  DragImpact,
+  DisplacedBy,
+  Displacement,
+} from '../../../../../src/types';
+import { vertical, horizontal } from '../../../../../src/state/axis';
+import { add, negate, subtract } from '../../../../../src/state/position';
+import scrollDroppable from '../../../../../src/state/droppable/scroll-droppable';
+import { getPreset, makeScrollable } from '../../../../utils/dimension';
+import getNotAnimatedDisplacement from '../../../../utils/get-displacement/get-not-animated-displacement';
+import getClientBorderBoxCenter from '../../../../../src/state/get-center-from-impact/get-client-border-box-center';
+import getDisplacedBy from '../../../../../src/state/get-displaced-by';
+import { forward } from '../../../../../src/state/user-direction/user-direction-preset';
+import noImpact from '../../../../../src/state/no-impact';
+import scrollViewport from '../../../../../src/state/scroll-viewport';
+import getHomeOnLift from '../../../../../src/state/get-home-on-lift';
+import getDisplacementMap from '../../../../../src/state/get-displacement-map';
+
+[vertical, horizontal].forEach((axis: Axis) => {
+  const preset = getPreset();
+  const { onLift, impact: homeImpact } = getHomeOnLift({
+    draggable: preset.inHome1,
+    draggables: preset.draggables,
+    home: preset.home,
+    viewport: preset.viewport,
+  });
+
+  it('should account for the scroll of the droppable you are over when reordering', () => {
+    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+    const scroll: Position = { x: 10, y: 15 };
+    const displacement: Position = negate(scroll);
+    const scrolled: DroppableDimension = scrollDroppable(
+      scrollableHome,
+      scroll,
+    );
+
+    const newClientCenter: Position = getClientBorderBoxCenter({
+      impact: homeImpact,
+      draggable: preset.inHome1,
+      droppable: scrolled,
+      draggables: preset.draggables,
+      viewport: preset.viewport,
+      onLift,
+    });
+    const offset: Position = subtract(
+      newClientCenter,
+      preset.inHome1.client.borderBox.center,
+    );
+
+    expect(offset).toEqual(displacement);
+  });
+
+  it('should account for the scroll of the droppable you are over when combining', () => {
+    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+    const scroll: Position = { x: 10, y: 15 };
+    const displacement: Position = negate(scroll);
+    const scrolled: DroppableDimension = scrollDroppable(
+      scrollableHome,
+      scroll,
+    );
+    // inHome1 combining with inHome2
+    const displacedBy: DisplacedBy = getDisplacedBy(
+      axis,
+      preset.inHome1.displaceBy,
+    );
+    const displaced: Displacement[] = [
+      getNotAnimatedDisplacement(preset.inHome2),
+      getNotAnimatedDisplacement(preset.inHome3),
+      getNotAnimatedDisplacement(preset.inHome4),
+    ];
+    const impact: DragImpact = {
+      movement: {
+        displaced,
+        map: getDisplacementMap(displaced),
+        displacedBy,
+      },
+      direction: axis.direction,
+      destination: null,
+      merge: {
+        whenEntered: forward,
+        combine: {
+          draggableId: preset.inHome2.descriptor.id,
+          droppableId: preset.home.descriptor.id,
+        },
+      },
+    };
+
+    const newClientCenter: Position = getClientBorderBoxCenter({
+      impact,
+      draggable: preset.inHome1,
+      draggables: preset.draggables,
+      droppable: scrolled,
+      viewport: preset.viewport,
+      onLift,
+    });
+    const offset: Position = subtract(
+      newClientCenter,
+      preset.inHome1.client.borderBox.center,
+    );
+
+    const expectedCenter: Position = preset.inHome2.client.borderBox.center;
+    const original: Position = preset.inHome1.client.borderBox.center;
+    const centerDiff: Position = subtract(expectedCenter, original);
+    const expectedOffset: Position = add(centerDiff, displacement);
+    expect(offset).toEqual(expectedOffset);
+  });
+
+  it('should account for the scroll of your home list if you are not over any list', () => {
+    const scrollableHome: DroppableDimension = makeScrollable(preset.home);
+    const scroll: Position = { x: 10, y: 15 };
+    const displacement: Position = negate(scroll);
+    const scrolled: DroppableDimension = scrollDroppable(
+      scrollableHome,
+      scroll,
+    );
+
+    const newClientCenter: Position = getClientBorderBoxCenter({
+      // over nothing
+      impact: noImpact,
+      draggable: preset.inHome1,
+      draggables: preset.draggables,
+      droppable: scrolled,
+      viewport: preset.viewport,
+      onLift,
+    });
+    const offset: Position = subtract(
+      newClientCenter,
+      preset.inHome1.client.borderBox.center,
+    );
+
+    expect(offset).toEqual(displacement);
+  });
+
+  it('should account for any changes in the window scroll', () => {
+    const scroll: Position = { x: 10, y: 15 };
+    const displacement: Position = negate(scroll);
+    const scrolled: Viewport = scrollViewport(
+      preset.viewport,
+      // adding to the existing scroll
+      add(preset.windowScroll, scroll),
+    );
+
+    const newClientCenter: Position = getClientBorderBoxCenter({
+      impact: noImpact,
+      draggable: preset.inHome1,
+      draggables: preset.draggables,
+      droppable: preset.home,
+      viewport: scrolled,
+      onLift,
+    });
+    const offset: Position = subtract(
+      newClientCenter,
+      preset.inHome1.client.borderBox.center,
+    );
+
+    expect(offset).toEqual(displacement);
+  });
+});
