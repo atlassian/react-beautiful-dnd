1f542d1f2ad6103ddc8995d483c628b659804b48
Alex Reardon <areardon@atlassian.com>
2019-02-22 08:49:02 +1100

    recompute tests

--- test/unit/state/update-displacement-visibility/recompute.spec.js
+++ test/unit/state/update-displacement-visibility/recompute.spec.js
@@ -6,67 +6,92 @@ import type {
   DragImpact,
 } from '../../../../src/types';
 import { getPreset } from '../../../utils/dimension';
-import getNotVisibleDisplacement from '../../../utils/get-not-visible-displacement';
+import getNotAnimatedDisplacement from '../../../utils/get-displacement/get-not-animated-displacement';
+import getNotVisibleDisplacement from '../../../utils/get-displacement/get-not-visible-displacement';
+import getVisibleDisplacement from '../../../utils/get-displacement/get-visible-displacement';
 import getDisplacedBy from '../../../../src/state/get-displaced-by';
 import getDisplacementMap from '../../../../src/state/get-displacement-map';
+import getHomeOnLift from '../../../../src/state/get-home-on-lift';
 import recompute from '../../../../src/state/update-displacement-visibility/recompute';
 import { horizontal, vertical } from '../../../../src/state/axis';
 
 [horizontal, vertical].forEach((axis: Axis) => {
   describe(`on ${axis.direction} axis`, () => {
     const preset = getPreset(axis);
+    // moving inHome1 down past inHome2 and inHome3
+    const { onLift } = getHomeOnLift({
+      draggable: preset.inHome1,
+      draggables: preset.draggables,
+      viewport: preset.viewport,
+      home: preset.home,
+    });
+    const displacedBy: DisplacedBy = getDisplacedBy(
+      axis,
+      preset.inHome1.displaceBy,
+    );
+    const initial: Displacement[] = [
+      // pretending the displacement is not visible
+      getNotVisibleDisplacement(preset.inHome2),
+      getNotVisibleDisplacement(preset.inHome3),
+    ];
+    const impact: DragImpact = {
+      movement: {
+        displacedBy,
+        displaced: initial,
+        map: getDisplacementMap(initial),
+      },
+      direction: axis.direction,
+      merge: null,
+      destination: {
+        droppableId: preset.home.descriptor.id,
+        index: preset.inHome3.descriptor.index,
+      },
+    };
+
     it('should recompute a displacement', () => {
-      // moving inHome1 down past inHome2 and inHome3
-
-      const willDisplaceForward: boolean = false;
-      const displacedBy: DisplacedBy = getDisplacedBy(
-        axis,
-        preset.inHome1.displaceBy,
-        willDisplaceForward,
-      );
-      const initial: Displacement[] = [
-        getNotVisibleDisplacement(preset.inHome3),
-        getNotVisibleDisplacement(preset.inHome2),
-      ];
-      const impact: DragImpact = {
-        movement: {
-          willDisplaceForward,
-          displacedBy,
-          displaced: initial,
-          map: getDisplacementMap(initial),
-        },
-        direction: axis.direction,
-        merge: null,
-        destination: {
-          droppableId: preset.home.descriptor.id,
-          index: preset.inHome3.descriptor.index,
-        },
-      };
-
       const recomputed: DragImpact = recompute({
         impact,
         viewport: preset.viewport,
         destination: preset.home,
         draggables: preset.draggables,
+        onLift,
       });
 
       const displaced: Displacement[] = [
-        {
-          draggableId: preset.inHome3.descriptor.id,
-          // was previously not visible so will not animate
-          shouldAnimate: false,
-          isVisible: true,
-        },
-        {
-          draggableId: preset.inHome2.descriptor.id,
-          shouldAnimate: false,
-          isVisible: true,
-        },
+        // visibility recalculated
+        getNotAnimatedDisplacement(preset.inHome2),
+        getNotAnimatedDisplacement(preset.inHome3),
       ];
       const expected: DragImpact = {
         ...impact,
         movement: {
-          willDisplaceForward,
+          displacedBy,
+          displaced,
+          map: getDisplacementMap(displaced),
+        },
+      };
+      expect(recomputed).toEqual(expected);
+    });
+
+    it('should allow the displacement animation to be forced', () => {
+      const recomputed: DragImpact = recompute({
+        impact,
+        viewport: preset.viewport,
+        destination: preset.home,
+        draggables: preset.draggables,
+        onLift,
+        forceShouldAnimate: true,
+      });
+
+      const displaced: Displacement[] = [
+        // visibility recalculated
+        // forced animation
+        getVisibleDisplacement(preset.inHome2),
+        getVisibleDisplacement(preset.inHome3),
+      ];
+      const expected: DragImpact = {
+        ...impact,
+        movement: {
           displacedBy,
           displaced,
           map: getDisplacementMap(displaced),
