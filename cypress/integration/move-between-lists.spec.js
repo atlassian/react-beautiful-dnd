c99da4a94068aff4728708dda8723f6372743aa8
Alex Reardon <alexreardon@users.noreply.github.com>
2019-02-27 10:25:38 +1100

    testcafe => Cypress (#1122)

---
+++ cypress/integration/move-between-lists.spec.js
@@ -0,0 +1,38 @@
+// @flow
+import * as keyCodes from '../../src/view/key-codes';
+import { timings } from '../../src/animation';
+
+beforeEach(() => {
+  cy.visit('/iframe.html?selectedKind=board&selectedStory=simple');
+});
+
+it('should move between lists', () => {
+  // first list has item with id:2
+  cy.get('[data-react-beautiful-dnd-droppable]')
+    .eq(1)
+    .as('first-list')
+    .should('contain', 'id:2');
+
+  // second list does not have item with id:2
+  cy.get('[data-react-beautiful-dnd-droppable]')
+    .eq(2)
+    .as('second-list')
+    .should('not.contain', 'id:2');
+
+  cy.get('@first-list')
+    .find('[data-react-beautiful-dnd-drag-handle]')
+    .first()
+    .should('contain', 'id:2')
+    .focus()
+    .trigger('keydown', { keyCode: keyCodes.space })
+    .trigger('keydown', { keyCode: keyCodes.arrowRight, force: true })
+    // finishing before the movement time is fine - but this looks nice
+    .wait(timings.outOfTheWay * 1000)
+    .trigger('keydown', { keyCode: keyCodes.space, force: true });
+
+  // no longer in the first list
+  cy.get('@first-list').should('not.contain', 'id:2');
+
+  // now in the second list
+  cy.get('@second-list').should('contain', 'id:2');
+});
