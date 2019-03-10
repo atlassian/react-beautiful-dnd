e453c618300ea2a23e553bc41d0bb16f57a19920
Alex Reardon <areardon@atlassian.com>
2019-03-10 12:58:50 +1100

    moving to storybook 5

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-import PopIframeDecorator from './decorator/pop-iframe';
+import { addParameters, configure, addDecorator } from '@storybook/react';
+import { create } from '@storybook/theming';
 import GlobalStylesDecorator from './decorator/global-styles';
 // adding css reset - storybook includes a css loader
 import '@atlaskit/css-reset';
+import { colors } from '@atlaskit/theme';
+import logo from './compressed-logo-rbd.svg';
 import { version } from '../package.json';
 
-addDecorator(
-  withOptions({
-    name: 'react-beautiful-dnd',
-    url: 'https://github.com/atlassian/react-beautiful-dnd',
-  }),
-);
+const theme = create({
+  brandImage: logo,
+  brandName: 'react-beautiful-dnd',
+  brandUrl: 'https://github.com/atlassian/react-beautiful-dnd',
+});
 
+addParameters({
+  options: {
+    // currently not using any addons
+    showPanel: false,
+    theme,
+  },
+});
+
+// TODO: use theme
+// It looks like the docs are a bit out of sync for v5 so i'll leave this for now
 addDecorator(GlobalStylesDecorator);
-addDecorator(PopIframeDecorator);
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories/', true, /.stories.js$/);
