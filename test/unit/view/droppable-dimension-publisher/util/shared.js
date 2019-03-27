e557764c26526f16e9b0f3d20c1eac0d49ab4126
Alex Reardon <areardon@atlassian.com>
2019-03-28 10:21:30 +1100

    test push forward

--- test/unit/view/droppable-dimension-publisher/util/shared.js
+++ test/unit/view/droppable-dimension-publisher/util/shared.js
@@ -142,141 +142,68 @@ export function ScrollableItem(props: ScrollableItemProps) {
   );
 }
 
-// export class ScrollableItem extends React.Component<ScrollableItemProps> {
-//   static defaultProps = {
-//     isScrollable: true,
-//     type: descriptor.type,
-//     droppableId: descriptor.id,
-//     isDropDisabled: false,
-//     isCombineEnabled: false,
-//   };
-//   /* eslint-disable react/sort-comp */
-//   ref: ?HTMLElement;
-//   placeholderRef: ?HTMLElement;
-
-//   setRef = (ref: ?HTMLElement) => {
-//     this.ref = ref;
-//   };
-
-//   setPlaceholderRef = (ref: ?HTMLElement) => {
-//     this.placeholderRef = ref;
-//   };
-
-//   getRef = (): ?HTMLElement => this.ref;
-//   getPlaceholderRef = (): ?HTMLElement => this.placeholderRef;
-
-//   render() {
-//     return (
-//       <DroppableDimensionPublisher
-//         droppableId={this.props.droppableId}
-//         type={this.props.type}
-//         direction={preset.home.axis.direction}
-//         isDropDisabled={this.props.isDropDisabled}
-//         ignoreContainerClipping={false}
-//         getDroppableRef={this.getRef}
-//         getPlaceholderRef={this.getPlaceholderRef}
-//         isCombineEnabled={this.props.isCombineEnabled}
-//       >
-//         <div
-//           className="scroll-container"
-//           style={{
-//             boxSizing: 'border-box',
-//             height: bigClient.borderBox.height,
-//             width: bigClient.borderBox.width,
-//             ...withSpacing,
-//             overflowX: this.props.isScrollable ? 'scroll' : 'visible',
-//             overflowY: this.props.isScrollable ? 'scroll' : 'visible',
-//           }}
-//           ref={this.setRef}
-//         >
-//           hi
-//           <div className="placeholder" ref={this.setPlaceholderRef} />
-//         </div>
-//       </DroppableDimensionPublisher>
-//     );
-//   }
-// }
-
 type AppProps = {|
-  droppableIsScrollable: boolean,
-  parentIsScrollable: boolean,
-  ignoreContainerClipping: boolean,
-  showPlaceholder: boolean,
+  droppableIsScrollable?: boolean,
+  parentIsScrollable?: boolean,
+  ignoreContainerClipping?: boolean,
+  showPlaceholder?: boolean,
 |};
 
-export class App extends React.Component<AppProps> {
-  ref: ?HTMLElement;
-  placeholderRef: ?HTMLElement;
+export function App(props: AppProps) {
+  const droppableRef = createRef();
+  const placeholderRef = createRef();
 
-  static defaultProps = {
-    ignoreContainerClipping: false,
-    droppableIsScrollable: false,
-    parentIsScrollable: false,
-    showPlaceholder: false,
-  };
+  const {
+    droppableIsScrollable = false,
+    parentIsScrollable = false,
+    ignoreContainerClipping = false,
+    showPlaceholder = false,
+  } = props;
 
-  setRef = (ref: ?HTMLElement) => {
-    this.ref = ref;
-  };
+  useDroppableDimensionPublisher({
+    droppableId: descriptor.id,
+    direction: 'vertical',
+    isDropDisabled: false,
+    isCombineEnabled: false,
+    type: descriptor.type,
+    ignoreContainerClipping,
+    getDroppableRef: droppableRef.getRef,
+    getPlaceholderRef: placeholderRef.getRef,
+  });
 
-  setPlaceholderRef = (ref: ?HTMLElement) => {
-    this.placeholderRef = ref;
-  };
-
-  getRef = (): ?HTMLElement => this.ref;
-  getPlaceholderRef = (): ?HTMLElement => this.placeholderRef;
-
-  render() {
-    const {
-      droppableIsScrollable,
-      parentIsScrollable,
-      ignoreContainerClipping,
-    } = this.props;
-    return (
-      <div
-        className="scroll-parent"
-        style={{
-          boxSizing: 'border-box',
-          height: smallFrameClient.borderBox.height,
-          width: smallFrameClient.borderBox.width,
-          ...withSpacing,
-          // setting both manually. This will be done automatically if setting overflow: scroll
-          overflowX: parentIsScrollable ? 'scroll' : 'visible',
-          overflowY: parentIsScrollable ? 'scroll' : 'visible',
-        }}
-      >
-        <div>
-          <div
-            ref={this.setRef}
-            className="droppable"
-            style={{
-              boxSizing: 'border-box',
-              height: bigClient.borderBox.height,
-              width: bigClient.borderBox.width,
-              ...withSpacing,
-              // setting both manually. This will be done automatically if setting overflow: scroll
-              overflowX: droppableIsScrollable ? 'scroll' : 'visible',
-              overflowY: droppableIsScrollable ? 'scroll' : 'visible',
-            }}
-          >
-            <DroppableDimensionPublisher
-              droppableId={descriptor.id}
-              direction="vertical"
-              isDropDisabled={false}
-              isCombineEnabled={false}
-              type={descriptor.type}
-              ignoreContainerClipping={ignoreContainerClipping}
-              getDroppableRef={this.getRef}
-              getPlaceholderRef={this.getPlaceholderRef}
-            >
-              <div>hello world</div>
-              {this.props.showPlaceholder ? (
-                <div className="placeholder" ref={this.setPlaceholderRef} />
-              ) : null}
-            </DroppableDimensionPublisher>
-          </div>
+  return (
+    <div
+      className="scroll-parent"
+      style={{
+        boxSizing: 'border-box',
+        height: smallFrameClient.borderBox.height,
+        width: smallFrameClient.borderBox.width,
+        ...withSpacing,
+        // setting both manually. This will be done automatically if setting overflow: scroll
+        overflowX: parentIsScrollable ? 'scroll' : 'visible',
+        overflowY: parentIsScrollable ? 'scroll' : 'visible',
+      }}
+    >
+      <div>
+        <div
+          ref={droppableRef.setRef}
+          className="droppable"
+          style={{
+            boxSizing: 'border-box',
+            height: bigClient.borderBox.height,
+            width: bigClient.borderBox.width,
+            ...withSpacing,
+            // setting both manually. This will be done automatically if setting overflow: scroll
+            overflowX: droppableIsScrollable ? 'scroll' : 'visible',
+            overflowY: droppableIsScrollable ? 'scroll' : 'visible',
+          }}
+        >
+          <div>hello world</div>
+          {showPlaceholder ? (
+            <div className="placeholder" ref={placeholderRef.setRef} />
+          ) : null}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
