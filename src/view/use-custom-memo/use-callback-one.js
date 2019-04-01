864b372de7513e56f4470c7ac47272e73144c0c4
Alex Reardon <areardon@atlassian.com>
2019-04-02 07:24:45 +1100

    useCallbackOne now using useMemoOne

--- src/view/use-custom-memo/use-callback-one.js
+++ src/view/use-custom-memo/use-callback-one.js
@@ -1,11 +1,5 @@
 // @flow
-import { useRef, useState, useEffect } from 'react';
-import areInputsEqual from './are-inputs-equal';
-
-type Result<T> = {|
-  inputs: mixed[],
-  callback: T,
-|};
+import useMemoOne from './use-memo-one';
 
 export default function useCallbackOne<T: Function>(
   // getResult changes on every call,
@@ -13,28 +7,5 @@ export default function useCallbackOne<T: Function>(
   // the inputs array changes on every call
   inputs?: mixed[] = [],
 ): T {
-  // using useState to generate initial value as it is lazy
-  const initial: Result<T> = useState(() => ({
-    inputs,
-    callback,
-  }))[0];
-
-  const uncommitted = useRef<Result<T>>(initial);
-  const committed = useRef<Result<T>>(initial);
-
-  // persist any uncommitted changes after they have been committed
-  useEffect(() => {
-    committed.current = uncommitted.current;
-  });
-
-  if (areInputsEqual(inputs, committed.current.inputs)) {
-    return committed.current.callback;
-  }
-
-  uncommitted.current = {
-    inputs,
-    callback,
-  };
-
-  return uncommitted.current.callback;
+  return useMemoOne(() => callback, inputs);
 }
