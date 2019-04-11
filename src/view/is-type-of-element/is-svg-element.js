9c0b92309bd441f941def88d728776db2aae5776
Alex Reardon <areardon@atlassian.com>
2019-04-12 08:38:39 +1000

    no longer crashing if SVGElement is not present

--- src/view/is-type-of-element/is-svg-element.js
+++ src/view/is-type-of-element/is-svg-element.js
@@ -2,5 +2,11 @@
 import getWindowFromEl from '../window/get-window-from-el';
 
 export default function isSvgElement(el: Object): boolean %checks {
-  return el instanceof getWindowFromEl(el).SVGElement;
+  // Some environments do not support SVGElement
+  // Doing a double lookup rather than storing the window
+  // as a %checks function can only be a 'simple predicate'
+  return (
+    Boolean(getWindowFromEl(el).SVGElement) &&
+    el instanceof getWindowFromEl(el).SVGElement
+  );
 }
