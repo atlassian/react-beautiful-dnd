c00e6e12885bb2227caf668b2cef1b81232e230a
Alex Reardon <areardon@atlassian.com>
2019-04-26 11:10:50 +1000

    fixing useIsomorephicLayoutEffect

--- src/view/use-isomorphic-layout-effect.js
+++ src/view/use-isomorphic-layout-effect.js
@@ -8,7 +8,7 @@ import { useLayoutEffect, useEffect } from 'react';
 // useLayoutEffect in the browser. We need useLayoutEffect because we want
 // `connect` to perform sync updates to a ref to save the latest props after
 // a render is actually committed to the DOM.
-const useLayoutEffect =
+const useIsomorphicLayoutEffect =
   typeof window !== 'undefined' ? useLayoutEffect : useEffect;
 
-export default useLayoutEffect;
+export default useIsomorphicLayoutEffect;
