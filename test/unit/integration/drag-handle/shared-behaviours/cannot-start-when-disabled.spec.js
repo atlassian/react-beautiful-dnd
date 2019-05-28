bddb7a7024f9fc14e0bf1f0a423a99ad8d7f5a93
Alex Reardon <areardon@atlassian.com>
2019-05-29 08:42:44 +1000

    shared tests for blocking drag start

---
+++ test/unit/integration/drag-handle/shared-behaviours/cannot-start-when-disabled.spec.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react';
+import { render } from 'react-testing-library';
+import { isDragging } from '../util';
+import App, { type Item } from '../app';
+import { forEachSensor, type Control, simpleLift } from './controls';
+
+forEachSensor((control: Control) => {
+  it('should not start a drag if disabled', () => {
+    const items: Item[] = [{ id: '0', isEnabled: false }];
+
+    const { getByText } = render(<App items={items} />);
+    const handle: HTMLElement = getByText('item: 0');
+
+    simpleLift(control, handle);
+
+    expect(isDragging(handle)).toBe(false);
+  });
+});
---
+++ test/unit/integration/drag-handle/shared-behaviours/cannot-start-when-something-else-has-lock.spec.js
@@ -0,0 +1,26 @@
+// @flow
+import invariant from 'tiny-invariant';
+import React from 'react';
+import { render } from 'react-testing-library';
+import { isDragging } from '../util';
+import App from '../app';
+import { forEachSensor, type Control, simpleLift } from './controls';
+
+forEachSensor((control: Control) => {
+  it('should not start a drag if another sensor is capturing', () => {
+    let tryGetLock;
+    function greedy(tryStartCapture) {
+      tryGetLock = tryStartCapture;
+    }
+    const { getByText } = render(<App sensors={[greedy]} />);
+    const handle: HTMLElement = getByText('item: 0');
+
+    invariant(tryGetLock, 'Expected function to be set');
+    tryGetLock(handle);
+
+    // won't be able to lift as the lock is already claimed
+    simpleLift(control, handle);
+
+    expect(isDragging(handle)).toBe(false);
+  });
+});
