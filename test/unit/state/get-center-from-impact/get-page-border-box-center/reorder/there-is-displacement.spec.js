e3033aad7b8363948e0ee25bf20cdc7db671f4d9
Alex Reardon <areardon@atlassian.com>
2019-07-01 17:01:00 +1000

    fixing tests

--- test/unit/state/get-center-from-impact/get-page-border-box-center/reorder/there-is-displacement.spec.js
+++ test/unit/state/get-center-from-impact/get-page-border-box-center/reorder/there-is-displacement.spec.js
@@ -9,12 +9,11 @@ import type {
 } from '../../../../../../src/types';
 import { vertical, horizontal } from '../../../../../../src/state/axis';
 import getPageBorderBoxCenter from '../../../../../../src/state/get-center-from-impact/get-page-border-box-center';
-import getHomeOnLift from '../../../../../../src/state/get-home-on-lift';
+import getLiftEffect from '../../../../../../src/state/get-lift-effect';
 import { getPreset } from '../../../../../utils/dimension';
 import { goBefore } from '../../../../../../src/state/get-center-from-impact/move-relative-to';
 import getDisplacedBy from '../../../../../../src/state/get-displaced-by';
-import getDisplacementMap from '../../../../../../src/state/get-displacement-map';
-import getNotAnimatedDisplacement from '../../../../../utils/get-displacement/get-not-animated-displacement';
+import { getForcedDisplacementGroups } from '../../../../../utils/impact';
 
 [vertical, horizontal].forEach((axis: Axis) => {
   describe(`on ${axis.direction} axis`, () => {
@@ -22,7 +21,7 @@ import getNotAnimatedDisplacement from '../../../../../utils/get-displacement/ge
 
     describe('closest displaced started displaced', () => {
       it('should go before the visible (displaced) position of the item', () => {
-        const { onLift } = getHomeOnLift({
+        const { afterCritical } = getLiftEffect({
           draggable: preset.inHome1,
           home: preset.home,
           draggables: preset.draggables,
@@ -32,27 +31,25 @@ import getNotAnimatedDisplacement from '../../../../../utils/get-displacement/ge
           axis,
           preset.inHome1.displaceBy,
         );
-        // moved forward over inHome2
-        const displaced: Displacement[] = [
-          getNotAnimatedDisplacement(preset.inHome3),
-          getNotAnimatedDisplacement(preset.inHome4),
-        ];
+
         const impact: DragImpact = {
-          movement: {
-            displacedBy,
-            displaced,
-            map: getDisplacementMap(displaced),
+          // moved forward over inHome2
+          displaced: getForcedDisplacementGroups({
+            visible: [preset.inHome3, preset.inHome4],
+            animation: [false, false],
+          }),
+          displacedBy,
+          at: {
+            type: 'REORDER',
+            destination: {
+              index: preset.inHome3.descriptor.index,
+              droppableId: preset.inHome3.descriptor.id,
+            },
           },
-          // currently in position of inHome3
-          destination: {
-            index: preset.inHome3.descriptor.index,
-            droppableId: preset.inHome3.descriptor.id,
-          },
-          merge: null,
         };
         const result: Position = getPageBorderBoxCenter({
           impact,
-          onLift,
+          afterCritical,
           draggable: preset.inHome1,
           draggables: preset.draggables,
           droppable: preset.home,
@@ -69,7 +66,7 @@ import getNotAnimatedDisplacement from '../../../../../utils/get-displacement/ge
 
     describe('closest displaced did not start displaced', () => {
       it('should go before the displaced position of the item', () => {
-        const { onLift } = getHomeOnLift({
+        const { afterCritical } = getLiftEffect({
           draggable: preset.inHome1,
           home: preset.home,
           draggables: preset.draggables,
@@ -80,27 +77,24 @@ import getNotAnimatedDisplacement from '../../../../../utils/get-displacement/ge
           preset.inHome1.displaceBy,
         );
         // moved into foreign
-        const displaced: Displacement[] = [
-          getNotAnimatedDisplacement(preset.inForeign2),
-          getNotAnimatedDisplacement(preset.inForeign3),
-          getNotAnimatedDisplacement(preset.inForeign4),
-        ];
         const impact: DragImpact = {
-          movement: {
-            displacedBy,
-            displaced,
-            map: getDisplacementMap(displaced),
+          displaced: getForcedDisplacementGroups({
+            visible: [preset.inForeign2, preset.inForeign3, preset.inForeign4],
+            animation: [false, false, false],
+          }),
+          displacedBy,
+          at: {
+            type: 'REORDER',
+            // currently in position of inForeign2
+            destination: {
+              index: preset.inForeign2.descriptor.index,
+              droppableId: preset.inForeign2.descriptor.id,
+            },
           },
-          // currently in position of inForeign2
-          destination: {
-            index: preset.inForeign2.descriptor.index,
-            droppableId: preset.inForeign2.descriptor.id,
-          },
-          merge: null,
         };
         const result: Position = getPageBorderBoxCenter({
           impact,
-          onLift,
+          afterCritical,
           draggable: preset.inHome1,
           draggables: preset.draggables,
           droppable: preset.foreign,
