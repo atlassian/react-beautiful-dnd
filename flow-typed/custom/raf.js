6eda543a0183ebd9fde0858b8bfa054f716b2217
Alex Reardon <alexreardon@gmail.com>
2019-07-08 17:20:55 +1000

    fiixng flow types

--- flow-typed/custom/raf.js
+++ flow-typed/custom/raf.js
@@ -1,6 +1,10 @@
 declare function raf(callback: (timestamp: number) => void): AnimationFrameID;
 
 declare var requestAnimationFrame: {
-  step: () => void,
-  flush: () => void,
+  // TODO: export type Stub from raf-stub
+  add: (cb: Function) => number,
+  remove: (id: number) => void,
+  flush: (duration?: number) => void,
+  reset: () => void,
+  step: (steps?: number, duration?: number) => void
 } & typeof raf;
