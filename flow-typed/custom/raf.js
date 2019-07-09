bdd262fc00c3e418d1c06f1db32f3b5015022975
Alex Reardon <areardon@atlassian.com>
2019-07-09 11:36:23 +1000

    better comment for raf type

--- flow-typed/custom/raf.js
+++ flow-typed/custom/raf.js
@@ -1,10 +1,11 @@
 declare function raf(callback: (timestamp: number) => void): AnimationFrameID;
 
+// TODO: would like to use `import type {Stub} from 'raf-stub'
+// This is not supported right now: https://github.com/flow-typed/flow-typed/issues/2023
 declare var requestAnimationFrame: {
-  // TODO: export type Stub from raf-stub
   add: (cb: Function) => number,
   remove: (id: number) => void,
   flush: (duration?: number) => void,
   reset: () => void,
-  step: (steps?: number, duration?: number) => void
+  step: (steps?: number, duration?: number) => void,
 } & typeof raf;
