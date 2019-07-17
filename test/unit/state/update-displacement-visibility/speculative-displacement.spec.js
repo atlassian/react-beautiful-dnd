132fab1a124d272095c72f02b3feb8006782a1d5
Alex Reardon <alexreardon@gmail.com>
2019-07-17 20:06:04 +1000

    cleanup file structure

--- test/unit/state/update-displacement-visibility/speculative-displacement.spec.js
+++ test/unit/state/update-displacement-visibility/speculative-displacement.spec.js
@@ -10,7 +10,6 @@ import type {
   DroppableDimension,
   DisplacedBy,
   DraggableDimensionMap,
-  Displacement,
 } from '../../../../src/types';
 import {
   getPreset,
@@ -23,19 +22,16 @@ import { createViewport } from '../../../utils/viewport';
 import { origin, patch } from '../../../../src/state/position';
 import { vertical, horizontal } from '../../../../src/state/axis';
 import { toDraggableMap } from '../../../../src/state/dimension-structures';
-import getDisplacementMap from '../../../../src/state/get-displacement-map';
 import getDisplacedBy from '../../../../src/state/get-displaced-by';
-import getVisibleDisplacement from '../../../utils/get-displacement/get-visible-displacement';
-import getNotVisibleDisplacement from '../../../utils/get-displacement/get-not-visible-displacement';
-import getNotAnimatedDisplacement from '../../../utils/get-displacement/get-not-animated-displacement';
 import { isPartiallyVisible } from '../../../../src/state/visibility/is-visible';
 import getLiftEffect from '../../../../src/state/get-lift-effect';
+import { getForcedDisplacement } from '../../../utils/impact';
 
 [vertical, horizontal].forEach((axis: Axis) => {
   describe(`on ${axis.direction} axis`, () => {
     it('should do nothing when there is no displacement', () => {
       const preset = getPreset(axis);
-      const { onLift, impact: homeImpact } = getHomeOnLift({
+      const { impact: homeImpact } = getLiftEffect({
         draggable: preset.inHome1,
         draggables: preset.draggables,
         home: preset.home,
@@ -48,7 +44,6 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         destination: preset.home,
         draggables: preset.draggables,
         maxScrollChange: { x: 1000, y: 1000 },
-        onLift,
       });
       expect(impact1).toEqual(homeImpact);
 
@@ -58,7 +53,6 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         destination: preset.home,
         draggables: preset.draggables,
         maxScrollChange: { x: 1000, y: 1000 },
-        onLift,
       });
       expect(impact2).toEqual(noImpact);
     });
@@ -74,7 +68,7 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
     // would normally be visible in viewport
     const sizeOfInForeign1: number = sizeOfInHome1;
     const sizeOfInForeign2: number = sizeOfInHome1;
-    // would normally not be visible in viewport
+    // would normally NOT be visible in viewport
     const sizeOfInForeign3: number = 10;
     const sizeOfInForeign4: number = 60;
     const sizeOfInForeign5: number = 100;
@@ -83,6 +77,7 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
       descriptor: {
         id: 'home',
         type: typeId,
+        mode: 'STANDARD',
       },
       direction: axis.direction,
       borderBox: {
@@ -190,6 +185,7 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         descriptor: {
           id: foreignId,
           type: 'huge',
+          mode: 'STANDARD',
         },
         direction: axis.direction,
         borderBox: {
@@ -261,33 +257,22 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
 
       // inHome1 has moved into the foreign list below inForeign1
       const displacedBy: DisplacedBy = getDisplacedBy(axis, inHome1.displaceBy);
-      const { onLift } = getHomeOnLift({
-        draggable: inHome1,
-        draggables,
-        home,
-        viewport,
-      });
 
-      // moved inHome1 over foreign
-      const initial: Displacement[] = [
-        // would normally be visible in viewport
-        getVisibleDisplacement(inForeign2),
-        // normally not visible in viewport
-        getNotVisibleDisplacement(inForeign3),
-        getNotVisibleDisplacement(inForeign4),
-        getNotVisibleDisplacement(inForeign5),
-      ];
       const previousImpact: DragImpact = {
-        movement: {
-          displaced: initial,
-          map: getDisplacementMap(initial),
-          displacedBy,
+        displaced: getForcedDisplacement({
+          // would normally be visible in viewport
+          visible: [{ dimension: inForeign2 }],
+          // normally not visible in viewport
+          invisible: [inForeign3, inForeign4, inForeign5],
+        }),
+        displacedBy,
+        at: {
+          type: 'REORDER',
+          destination: {
+            droppableId: foreign.descriptor.id,
+            index: inForeign2.descriptor.index,
+          },
         },
-        destination: {
-          droppableId: foreign.descriptor.id,
-          index: inForeign2.descriptor.index,
-        },
-        merge: null,
       };
 
       const result: DragImpact = speculativelyIncrease({
@@ -296,28 +281,29 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         destination: foreign,
         draggables,
         maxScrollChange: patch(axis.line, sizeOfInHome1),
-        onLift,
       });
 
-      const displaced: Displacement[] = [
-        // already visibly displaced
-        getVisibleDisplacement(inForeign2),
-        // speculatively increased
-        getNotAnimatedDisplacement(inForeign3),
-        getNotAnimatedDisplacement(inForeign4),
-        // not speculatively increased
-        getNotVisibleDisplacement(inForeign5),
-      ];
+      // const displaced: Displacement[] = [
+      //   getVisibleDisplacement(inForeign2),
+      //   getNotAnimatedDisplacement(inForeign3),
+      //   getNotAnimatedDisplacement(inForeign4),
+      //   getNotVisibleDisplacement(inForeign5),
+      // ];
       const expected: DragImpact = {
         // unchanged locations
         ...previousImpact,
-        movement: {
-          displaced,
-          map: getDisplacementMap(displaced),
-          displacedBy,
-        },
+        displaced: getForcedDisplacement({
+          visible: [
+            // already visibly displaced
+            { dimension: inForeign2, shouldAnimate: true },
+            // speculatively increased. Forced to animate
+            { dimension: inForeign3, shouldAnimate: true },
+            { dimension: inForeign4, shouldAnimate: true },
+          ],
+          invisible: [inForeign5],
+        }),
       };
-      expect(expected).toEqual(result);
+      expect(result).toEqual(expected);
     });
 
     it('should increase the visible displacement in the droppable by the amount of the max scroll change', () => {
@@ -329,6 +315,7 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         descriptor: {
           id: foreignId,
           type: 'huge',
+          mode: 'STANDARD',
         },
         direction: axis.direction,
         // large subject
@@ -412,32 +399,22 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
 
       // inHome1 has moved into the foreign list below inForeign1
       const displacedBy: DisplacedBy = getDisplacedBy(axis, inHome1.displaceBy);
-      const { onLift } = getHomeOnLift({
-        draggable: inHome1,
-        draggables,
-        home,
-        viewport,
-      });
 
-      const initial: Displacement[] = [
-        // would normally be visible in viewport
-        getVisibleDisplacement(inForeign2),
-        // normally not visible in viewport
-        getNotVisibleDisplacement(inForeign3),
-        getNotVisibleDisplacement(inForeign4),
-        getNotVisibleDisplacement(inForeign5),
-      ];
       const previousImpact: DragImpact = {
-        movement: {
-          displaced: initial,
-          map: getDisplacementMap(initial),
-          displacedBy,
+        displaced: getForcedDisplacement({
+          // would normally be visible in viewport
+          visible: [{ dimension: inForeign2 }],
+          // normally not visible in viewport
+          invisible: [inForeign3, inForeign4, inForeign5],
+        }),
+        displacedBy,
+        at: {
+          type: 'REORDER',
+          destination: {
+            droppableId: foreign.descriptor.id,
+            index: inForeign2.descriptor.index,
+          },
         },
-        destination: {
-          droppableId: foreign.descriptor.id,
-          index: inForeign2.descriptor.index,
-        },
-        merge: null,
       };
 
       const result: DragImpact = speculativelyIncrease({
@@ -446,28 +423,24 @@ import getLiftEffect from '../../../../src/state/get-lift-effect';
         destination: foreign,
         draggables,
         maxScrollChange: patch(axis.line, sizeOfInHome1),
-        onLift,
       });
 
-      const displaced: Displacement[] = [
-        // already visibly displaced
-        getVisibleDisplacement(inForeign2),
-        // speculatively increased
-        getNotAnimatedDisplacement(inForeign3),
-        getNotAnimatedDisplacement(inForeign4),
-        // not speculatively increased
-        getNotVisibleDisplacement(inForeign5),
-      ];
       const expected: DragImpact = {
         // unchanged locations
         ...previousImpact,
-        movement: {
-          displaced,
-          map: getDisplacementMap(displaced),
-          displacedBy,
-        },
+        displaced: getForcedDisplacement({
+          visible: [
+            // already visibly displaced
+            { dimension: inForeign2, shouldAnimate: true },
+            // speculatively increased
+            { dimension: inForeign3, shouldAnimate: true },
+            { dimension: inForeign4, shouldAnimate: true },
+          ],
+          // not speculatively increased
+          invisible: [inForeign5],
+        }),
       };
-      expect(expected).toEqual(result);
+      expect(result).toEqual(expected);
     });
   });
 });
