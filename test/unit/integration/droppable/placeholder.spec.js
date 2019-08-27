2dc31ed52913d84b6e4f7ba2a7c5fbfe413731d3
Alex Reardon <alexreardon@gmail.com>
2019-08-27 17:29:55 +1000

    placeholder tests

--- test/unit/integration/droppable/placeholder.spec.js
+++ test/unit/integration/droppable/placeholder.spec.js
@@ -4,11 +4,12 @@ import invariant from 'tiny-invariant';
 import { render, fireEvent, act } from '@testing-library/react';
 import * as attributes from '../../../../src/view/data-attributes';
 import type { DroppableId } from '../../../../src/types';
-import { isOver } from '../util/helpers';
+import { isOver, isDragging } from '../util/helpers';
 import expandedMouse from '../util/expanded-mouse';
 import Board, { withPoorBoardDimensions } from '../util/board';
 import { toDroppableList } from '../../../../src/state/dimension-structures';
 import { getTransitionEnd } from '../util/controls';
+import { withError } from '../../../util/console';
 
 function findPlaceholder(
   droppableId: DroppableId,
@@ -63,7 +64,7 @@ it('should render a placeholder when dragging over', () => {
   });
 });
 
-it('should always render a placeholder in the home list while dragging', () => {
+it('should remove all placeholders if an error occurs while dragging', () => {
   withPoorBoardDimensions(preset => {
     toDroppableList(preset.droppables).forEach(droppable => {
       const { container, getByTestId, unmount } = render(<Board />);
@@ -73,44 +74,16 @@ it('should always render a placeholder in the home list while dragging', () => {
       expandedMouse.powerLift(handle, box0.center);
       expandedMouse.move(handle, droppable.client.borderBox.center);
 
-      // doesn't matter what we are over
+      expect(isDragging(handle)).toBe(true);
       expect(isOver(handle)).toBe(droppable.descriptor.id);
-      // there is always a placeholder in home
-      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
-
-      unmount();
-    });
-  });
-});
-
-it('should not render a placeholder in a foreign list if not dragging over', () => {
-  withPoorBoardDimensions(preset => {
-    const { container, getByTestId } = render(<Board />);
-    const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
-    const box0 = preset.inHome1.client.borderBox;
-
-    expandedMouse.powerLift(handle, box0.center);
-
-    expect(isOver(handle)).toBe(preset.home.descriptor.id);
-    expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(false);
-  });
-});
-
-it('should immediately remove the home placeholder after dropping into any list', () => {
-  withPoorBoardDimensions(preset => {
-    toDroppableList(preset.droppables).forEach(droppable => {
-      const { container, getByTestId, unmount } = render(<Board />);
-      const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
-
-      expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
-      expandedMouse.move(handle, droppable.client.borderBox.center);
-      expect(isOver(handle)).toBe(droppable.descriptor.id);
-
-      expandedMouse.startDrop(handle);
       expect(hasPlaceholder(droppable.descriptor.id, container)).toBe(true);
 
-      // placeholder removed straight after drop
-      expandedMouse.finishDrop(handle);
+      withError(() => {
+        const event: Event = new Event('error');
+        fireEvent(window, event);
+      });
+
+      expect(isDragging(handle)).toBe(false);
       expect(hasPlaceholder(droppable.descriptor.id, container)).toBe(false);
 
       unmount();
@@ -118,98 +91,239 @@ it('should immediately remove the home placeholder after dropping into any list'
   });
 });
 
-it('should immediately remove the home placeholder after dropping nowhere', () => {
-  withPoorBoardDimensions(preset => {
-    const { container, getByTestId } = render(<Board />);
-    const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+describe('home list', () => {
+  it('should always render a placeholder while dragging', () => {
+    withPoorBoardDimensions(preset => {
+      toDroppableList(preset.droppables).forEach(droppable => {
+        const { container, getByTestId, unmount } = render(<Board />);
+        const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+        const box0 = preset.inHome1.client.borderBox;
 
-    expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
-    expandedMouse.move(handle, { x: 10000, y: 10000 });
-    expect(isOver(handle)).toBe(null);
+        expandedMouse.powerLift(handle, box0.center);
+        expandedMouse.move(handle, droppable.client.borderBox.center);
 
-    // placeholder present when over nothing
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+        // doesn't matter what we are over
+        expect(isOver(handle)).toBe(droppable.descriptor.id);
+        // there is always a placeholder in home
+        expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
 
-    // placeholder present when drop started
-    expandedMouse.startDrop(handle);
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
-
-    // placeholder gone after drop finished
-    expandedMouse.finishDrop(handle);
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(false);
+        unmount();
+      });
+    });
   });
-});
 
-it('should animate the home placeholder closed after dropping into a foreign list', () => {
-  withPoorBoardDimensions(preset => {
-    const { container, getByTestId } = render(<Board />);
-    const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+  it('should immediately remove the home placeholder after dropping into any list', () => {
+    withPoorBoardDimensions(preset => {
+      toDroppableList(preset.droppables).forEach(droppable => {
+        const { container, getByTestId, unmount } = render(<Board />);
+        const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
 
-    expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
-    expandedMouse.move(handle, preset.inForeign1.client.borderBox.center);
-    expect(isOver(handle)).toBe(preset.foreign.descriptor.id);
+        expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
+        expandedMouse.move(handle, droppable.client.borderBox.center);
+        expect(isOver(handle)).toBe(droppable.descriptor.id);
 
-    expandedMouse.startDrop(handle);
-    // foreign placeholder remaining in place
-    expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(true);
-    // home placeholder remaining in place
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+        expandedMouse.startDrop(handle);
+        expect(hasPlaceholder(droppable.descriptor.id, container)).toBe(true);
 
-    // gone after drop finished
-    expandedMouse.finishDrop(handle);
+        // placeholder removed straight after drop
+        expandedMouse.finishDrop(handle);
+        expect(hasPlaceholder(droppable.descriptor.id, container)).toBe(false);
 
-    // foreign placeholder is now gone
-    expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(false);
-    // home placeholder is still around and will now animate closed
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
-
-    // placeholder is now collapsing
-    const placeholder: HTMLElement = getPlaceholder(
-      preset.home.descriptor.id,
-      container,
-    );
-    expect(placeholder.style.height).toBe('0px');
-
-    // faking a transition end
-    fireEvent(placeholder, getTransitionEnd('height'));
-
-    // placeholder is gone
-    expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(false);
+        unmount();
+      });
+    });
   });
-});
 
-it('should flush a home placeholder collapse animation if starting a new drag', () => {
-  withPoorBoardDimensions(preset => {
-    const { container, getByTestId } = render(<Board />);
-    {
+  it('should immediately remove the home placeholder after dropping nowhere', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
+      const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+
+      expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
+      expandedMouse.move(handle, { x: 10000, y: 10000 });
+      expect(isOver(handle)).toBe(null);
+
+      // placeholder present when over nothing
+      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+
+      // placeholder present when drop started
+      expandedMouse.startDrop(handle);
+      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+
+      // placeholder gone after drop finished
+      expandedMouse.finishDrop(handle);
+      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(false);
+    });
+  });
+
+  it('should animate the home placeholder closed after dropping into a foreign list', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
       const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
 
       expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
       expandedMouse.move(handle, preset.inForeign1.client.borderBox.center);
       expect(isOver(handle)).toBe(preset.foreign.descriptor.id);
 
-      expandedMouse.powerDrop(handle);
-      // placeholder still here + animating closed
+      expandedMouse.startDrop(handle);
+      // foreign placeholder remaining in place
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        true,
+      );
+      // home placeholder remaining in place
       expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+
+      // gone after drop finished
+      expandedMouse.finishDrop(handle);
+
+      // foreign placeholder is now gone
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        false,
+      );
+      // home placeholder is still around and will now animate closed
+      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+
+      // placeholder is now collapsing
       const placeholder: HTMLElement = getPlaceholder(
         preset.home.descriptor.id,
         container,
       );
-      // animating height closed
       expect(placeholder.style.height).toBe('0px');
-    }
-    {
-      const handle: HTMLElement = getByTestId(preset.inForeign1.descriptor.id);
 
-      // using raw power lift as we don't want to combine renders
-      // (we need a render to flush the placeholder out)
-      expandedMouse.rawPowerLift(
-        handle,
-        preset.inForeign1.client.borderBox.center,
+      // faking a transition end
+      fireEvent(placeholder, getTransitionEnd('height'));
+
+      // placeholder is gone
+      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(false);
+    });
+  });
+
+  it('should flush a home placeholder collapse animation if starting a new drag', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
+      {
+        const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+
+        expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
+        expandedMouse.move(handle, preset.inForeign1.client.borderBox.center);
+        expect(isOver(handle)).toBe(preset.foreign.descriptor.id);
+
+        expandedMouse.powerDrop(handle);
+        // placeholder still here + animating closed
+        expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(true);
+        const placeholder: HTMLElement = getPlaceholder(
+          preset.home.descriptor.id,
+          container,
+        );
+        // animating height closed
+        expect(placeholder.style.height).toBe('0px');
+      }
+      {
+        const handle: HTMLElement = getByTestId(
+          preset.inForeign1.descriptor.id,
+        );
+
+        // using raw power lift as we don't want to combine renders
+        // (we need a render to flush the placeholder out)
+        expandedMouse.rawPowerLift(
+          handle,
+          preset.inForeign1.client.borderBox.center,
+        );
+
+        // placeholder is gone from home (it got flushed)
+        expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(
+          false,
+        );
+      }
+    });
+  });
+});
+
+describe('foreign list', () => {
+  it('should not render a placeholder if not dragging over', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
+      const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+      const box0 = preset.inHome1.client.borderBox;
+
+      expandedMouse.powerLift(handle, box0.center);
+
+      expect(isOver(handle)).toBe(preset.home.descriptor.id);
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        false,
+      );
+    });
+  });
+
+  it('should animate a placeholder closed when no longer dragging over', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
+      const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+
+      expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
+      expandedMouse.move(handle, preset.foreign.client.borderBox.center);
+
+      expect(isOver(handle)).toBe(preset.foreign.descriptor.id);
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        true,
       );
 
-      // placeholder is gone from home (it got flushed)
-      expect(hasPlaceholder(preset.home.descriptor.id, container)).toBe(false);
-    }
+      // moving back over home
+      expandedMouse.move(handle, preset.home.client.borderBox.center);
+      expect(isOver(handle)).toBe(preset.home.descriptor.id);
+
+      // foreign placeholder is now animating closed
+      const placeholder: HTMLElement = getPlaceholder(
+        preset.foreign.descriptor.id,
+        container,
+      );
+      expect(placeholder.style.height).toBe('0px');
+    });
+  });
+
+  it('should flush a foreign list collapsing animation if a new drag starts', () => {
+    withPoorBoardDimensions(preset => {
+      const { container, getByTestId } = render(<Board />);
+      const handle: HTMLElement = getByTestId(preset.inHome1.descriptor.id);
+
+      expandedMouse.powerLift(handle, preset.inHome1.client.borderBox.center);
+      expandedMouse.move(handle, preset.foreign.client.borderBox.center);
+
+      expect(isOver(handle)).toBe(preset.foreign.descriptor.id);
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        true,
+      );
+
+      // moving back over home
+      expandedMouse.move(handle, preset.home.client.borderBox.center);
+      expect(isOver(handle)).toBe(preset.home.descriptor.id);
+
+      // foreign placeholder is now animating closed
+      const placeholder: HTMLElement = getPlaceholder(
+        preset.foreign.descriptor.id,
+        container,
+      );
+      expect(placeholder.style.height).toBe('0px');
+
+      // starting a drop into home
+      expandedMouse.startDrop(handle);
+      // placeholder still in foreign list
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        true,
+      );
+
+      const second: HTMLElement = getByTestId(preset.inHome2.descriptor.id);
+
+      // lifting item 2 while item 1 is dropping
+      expandedMouse.rawPowerLift(
+        second,
+        preset.inHome2.client.borderBox.center,
+      );
+      expect(isDragging(second)).toBe(true);
+      expect(isOver(second)).toBe(preset.home.descriptor.id);
+      expect(hasPlaceholder(preset.foreign.descriptor.id, container)).toBe(
+        false,
+      );
+    });
   });
 });
