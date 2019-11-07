636d12fc2ecc1a24676145dcc270feddec685d97
Alex Reardon <alexreardon@users.noreply.github.com>
2019-11-07 17:18:47 +1100

    Update test/unit/integration/accessibility/axe-audit.spec.js

--- test/unit/integration/accessibility/axe-audit.spec.js
+++ test/unit/integration/accessibility/axe-audit.spec.js
@@ -14,6 +14,7 @@ it('should not fail an aXe audit', async () => {
 
   const results = await axe(document.body);
 
+// $FlowFixMe - flow doesn't know about hte custom validator
   expect(results).toHaveNoViolations();
 
   cleanup();
