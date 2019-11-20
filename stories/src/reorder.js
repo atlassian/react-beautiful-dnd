45e395f78da92d8d03ac7eb216949645f6574c8d
Alex Reardon <alexreardon@users.noreply.github.com>
2019-11-21 09:11:42 +1100

    Wide lists experiment (#1619)

--- stories/src/reorder.js
+++ stories/src/reorder.js
@@ -65,3 +65,47 @@ export const reorderQuoteMap = ({
     quoteMap: result,
   };
 };
+
+type List<T> = {|
+  id: string,
+  values: T[],
+|};
+
+type MoveBetweenArgs<T> = {|
+  list1: List<T>,
+  list2: List<T>,
+  source: DraggableLocation,
+  destination: DraggableLocation,
+|};
+
+type MoveBetweenResult<T> = {|
+  list1: List<T>,
+  list2: List<T>,
+|};
+
+export function moveBetween<T>({
+  list1,
+  list2,
+  source,
+  destination,
+}: MoveBetweenArgs<T>): MoveBetweenResult<T> {
+  const newFirst = Array.from(list1.values);
+  const newSecond = Array.from(list2.values);
+
+  const moveFrom = source.droppableId === list1.id ? newFirst : newSecond;
+  const moveTo = moveFrom === newFirst ? newSecond : newFirst;
+
+  const [moved] = moveFrom.splice(source.index, 1);
+  moveTo.splice(destination.index, 0, moved);
+
+  return {
+    list1: {
+      ...list1,
+      values: newFirst,
+    },
+    list2: {
+      ...list2,
+      values: newSecond,
+    },
+  };
+}
