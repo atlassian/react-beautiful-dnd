62708e6b3fcf4659a52928ba557ddbe46d5e4576
Alex Reardon <alexreardon@gmail.com>
2019-11-29 16:58:49 +1100

    more test setup

--- test/unit/state/get-droppable-over/center-is-over.spec.js
+++ test/unit/state/get-droppable-over/center-is-over.spec.js
@@ -1,1 +1,58 @@
 // @flow
+import type { Position, Rect } from 'css-box-model';
+import type {
+  Axis,
+  DroppableId,
+  DraggableDimension,
+} from '../../../../src/types';
+import { vertical, horizontal } from '../../../../src/state/axis';
+import { getPreset } from '../../../util/dimension';
+import { getCorners, expandByPosition } from '../../../../src/state/spacing';
+import getDroppableOver from '../../../../src/state/get-droppable-over';
+import { invariant } from '../../../../src/invariant';
+import { subtract, patch } from '../../../../src/state/position';
+
+[vertical, horizontal].forEach((axis: Axis) => {
+  describe(`on ${axis.direction} axis`, () => {
+    const preset = getPreset(axis);
+    const active: ?Rect = preset.home.subject.active;
+    invariant(active, 'expected active');
+    const draggable: DraggableDimension = preset.inHome1;
+
+    it('should return a list when the center is over the list', () => {
+      const corners: Position[] = getCorners(active);
+
+      corners.forEach((corner: Position) => {
+        const pageOffset: Position = subtract(
+          corner,
+          draggable.page.borderBox.center,
+        );
+        const result: ?DroppableId = getDroppableOver({
+          pageOffset,
+          draggable,
+          droppables: preset.droppables,
+        });
+
+        expect(result).toBe(preset.home.descriptor.id);
+      });
+
+      const outside: Position[] = getCorners(
+        expandByPosition(active, patch(axis.line, 1)),
+      );
+
+      outside.forEach((corner: Position) => {
+        const pageOffset: Position = subtract(
+          corner,
+          draggable.page.borderBox.center,
+        );
+        const result: ?DroppableId = getDroppableOver({
+          pageOffset,
+          draggable,
+          droppables: preset.droppables,
+        });
+
+        expect(result).toBe(null);
+      });
+    });
+  });
+});
