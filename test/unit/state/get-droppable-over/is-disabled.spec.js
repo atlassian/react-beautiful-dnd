a341cc3fd3e3cc6e5aa8fc80dbb5f5c22bac5ddf
Alex Reardon <alexreardon@gmail.com>
2019-11-30 19:26:38 +1100

    fixing new tests

--- test/unit/state/get-droppable-over/is-disabled.spec.js
+++ test/unit/state/get-droppable-over/is-disabled.spec.js
@@ -2,7 +2,6 @@
 import type { DroppableDimensionMap, DroppableId } from '../../../../src/types';
 import getDroppableOver from '../../../../src/state/get-droppable-over';
 import { disableDroppable, getPreset } from '../../../util/dimension';
-import { origin } from '../../../../src/state/position';
 
 const preset = getPreset();
 
@@ -13,12 +12,12 @@ it('should not consider lists that are disabled', () => {
   };
 
   const whileEnabled: ?DroppableId = getDroppableOver({
-    pageOffset: origin,
+    pageBorderBox: preset.inHome1.page.borderBox,
     draggable: preset.inHome1,
     droppables: preset.droppables,
   });
   const whileDisabled: ?DroppableId = getDroppableOver({
-    pageOffset: origin,
+    pageBorderBox: preset.inHome1.page.borderBox,
     draggable: preset.inHome1,
     droppables: withDisabled,
   });
--- test/unit/state/get-droppable-over/is-over-nothing.spec.js
+++ test/unit/state/get-droppable-over/is-over-nothing.spec.js
@@ -2,12 +2,16 @@
 import type { DroppableId } from '../../../../src/types';
 import getDroppableOver from '../../../../src/state/get-droppable-over';
 import { getPreset } from '../../../util/dimension';
+import { offsetRectByPosition } from '../../../../src/state/rect';
 
 const preset = getPreset();
 
 it('should return null when over nothing', () => {
   const result: ?DroppableId = getDroppableOver({
-    pageOffset: { x: 10000, y: 10000 },
+    pageBorderBox: offsetRectByPosition(preset.inHome1.page.borderBox, {
+      x: 10000,
+      y: 10000,
+    }),
     draggable: preset.inHome1,
     droppables: preset.droppables,
   });
