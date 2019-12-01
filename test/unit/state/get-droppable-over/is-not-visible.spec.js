c765538043ef594aaada3720c722838a07942789
Alex Reardon <areardon@atlassian.com>
2019-12-02 09:24:37 +1100

    lint and comments

--- test/unit/state/get-droppable-over/is-not-visible.spec.js
+++ test/unit/state/get-droppable-over/is-not-visible.spec.js
@@ -1,4 +1,4 @@
 // @flow
-it('should not consider lists that have no visible area', () => {});
+it('should not consider lists that have no visible hitbox', () => {});
 
 it('should not allow results that are not in the currently visible area', () => {});
