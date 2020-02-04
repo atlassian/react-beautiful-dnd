adb3957fd6421db863dab4b7f121425a76ed2221
Alex Reardon <alexreardon@gmail.com>
2020-02-04 17:06:45 +1100

    fixing linting

--- stories/35-function-component.stories.js
+++ stories/35-function-component.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import QuoteApp from './src/function-component/quote-app';
 
-storiesOf('Function component usage', module).add(
-  'using rbd with function components and hooks',
-  () => <QuoteApp />,
-);
+storiesOf(
+  'Function component usage',
+  module,
+).add('using rbd with function components and hooks', () => <QuoteApp />);
