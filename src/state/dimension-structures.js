395dfc4d5758959d18140f2daf422446e1cd22a4
Gowthaman Ganesan <gowthaman.ganesan@mallow-tech.com>
2020-09-01 14:58:46 +0530

    Keeping the droppable active till it passes the folder

--- src/state/dimension-structures.js
+++ src/state/dimension-structures.js
@@ -24,6 +24,38 @@ export const toDraggableMap = memoizeOne(
     }, {}),
 );
 
+let oldActiveDroppable = '';
+
+export const getDroppableList = (draggable, pageBorderBox, droppables) => {
+  const draggableParentId =
+    oldActiveDroppable !== draggable.descriptor.droppableId
+      ? `${document.getElementById('appear-on-top').classList[0]}-folder-items`
+      : draggable.descriptor.droppableId;
+  const parentRndComponent = document.getElementsByClassName(
+    draggableParentId.replace('-folder-items', ''),
+  )[0];
+  const parentPosition = parentRndComponent.getBoundingClientRect();
+  let newDroppables = {};
+  const isInsideParent = !(
+    pageBorderBox.center.x > parentPosition.right ||
+    pageBorderBox.center.x < parentPosition.left ||
+    pageBorderBox.center.y < parentPosition.top ||
+    pageBorderBox.center.y > parentPosition.bottom
+  );
+
+  if (isInsideParent) {
+    newDroppables = {
+      [draggableParentId]: droppables[draggableParentId],
+      'new-folder': droppables['new-folder'],
+    };
+  } else {
+    oldActiveDroppable = `${
+      document.getElementById('appear-on-top').classList[0]
+    }-folder-items`;
+  }
+  return isInsideParent ? values(newDroppables) : values(droppables);
+};
+
 export const toDroppableList = memoizeOne(
   (droppables: DroppableDimensionMap): DroppableDimension[] =>
     values(droppables),
