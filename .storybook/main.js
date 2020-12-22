92690b42d6c2a246cc4494a6c5f0ad41984d339c
Daniel Westlund <dwestlund531@gmail.com>
2020-12-22 08:32:20 -0600

    removed config options that are not needed in 5.3.* storysource

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,3 @@
 module.exports = {
-  addons: [
-    'storybook-addon-performance/register',
-    {
-      name: '@storybook/addon-storysource',
-      options: {
-         sourceLoaderOptions: {
-          injectStoryParameters: false,
-        },
-        loaderOptions: {
-          prettierConfig: { printWidth: 80, singleQuote: false },
-        },
-      },
-    },
-  ],
+  addons: ['storybook-addon-performance/register','@storybook/addon-storysource'],
 };
